7179d2edcc1b29a2c08a3dce9c7fb24090df61e3
Nicolas Maquet <nmaquet@gmail.com>
2013-12-17 14:57:30 +0100

    python script starting to work :)

--- scripts/shorten_monsters.py
+++ scripts/shorten_monsters.py
@@ -1,14 +1,26 @@
 import sys
 import os.path
+import json
 
-if len(sys.argv) != 2:
-    print "usage : shorten_monsters.py <filepath>"
-    sys.exit(-1)
+def getPath():
+    if len(sys.argv) != 2:
+        print "usage : shorten_monsters.py <filepath>"
+        sys.exit(-1)
+    path = sys.argv[1]
+    if not os.path.exists(path):
+        print "fatal : could not find <filepath>"
+        sys.exit(-1)
+    return path
 
-path = sys.argv[1]
+def processPath(path):
+    with open(path) as f:
+        contents = f.read();
+    monsters = json.loads(contents);
+    short_monsters = []
+    for monster in monsters:
+        short_monsters.append({ "name" : monster.get("Name"), "id" : monster.get("id"), "cr" : monster.get("CR")});
+    print json.dumps(short_monsters);
 
-if not os.path.exists(path):
-    print "fatal : could not find <filepath>"
-    sys.exit(-1)
-
-print "processing", path
+if __name__ == "__main__":
+    path = getPath();
+    processPath(path)
