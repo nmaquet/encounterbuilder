8c6c7824faad497b1ac169801203cdbb3d305b47
Nicolas Maquet <nmaquet@gmail.com>
2013-12-17 16:26:36 +0100

    first version

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,13 +10,9 @@ encounterBuilderControllers.controller('MonsterListController', ['$scope', 'Mons
     $scope.orderProp = 'cr';
   }]);
 
-encounterBuilderControllers.controller('MonsterDetailController', ['$scope', '$routeParams', 'Monster',
-  function($scope, $routeParams, Monster) {
-    $scope.phone = Monster.get({monsterId: $routeParams.monsterId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
+encounterBuilderControllers.controller('MonsterDetailController', ['$scope', '$routeParams', '$sce', 'Monster',
+  function($scope, $routeParams, $sce, Monster) {
+    $scope.monster = Monster.get({monsterId: $routeParams.monsterId}, function(monster) {
+    $scope.monster.FullTextSafe = $sce.trustAsHtml($scope.monster.FullText);
     });
-
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    }
   }]);
