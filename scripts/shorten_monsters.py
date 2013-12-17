f0edc8511377dc24233dcef91b6b775056f90cf6
Nicolas Maquet <nmaquet@gmail.com>
2013-12-17 15:05:21 +0100

    shorten script done & shorted monster list commited

--- scripts/shorten_monsters.py
+++ scripts/shorten_monsters.py
@@ -2,25 +2,31 @@ import sys
 import os.path
 import json
 
-def getPath():
-    if len(sys.argv) != 2:
-        print "usage : shorten_monsters.py <filepath>"
+def getPaths():
+    if len(sys.argv) != 3:
+        print "usage : shorten_monsters.py <inPath> <outPath>"
         sys.exit(-1)
-    path = sys.argv[1]
-    if not os.path.exists(path):
-        print "fatal : could not find <filepath>"
+    inPath = sys.argv[1]
+    outPath = sys.argv[2]
+    if not os.path.exists(inPath):
+        print "fatal : could not find <inPath>"
         sys.exit(-1)
-    return path
+    if os.path.exists(outPath):
+        print "fatal : <outPath> already exists"
+        sys.exit(-1)
+    return inPath, outPath
 
-def processPath(path):
-    with open(path) as f:
-        contents = f.read();
-    monsters = json.loads(contents);
+def process(inPath, outPath):
+    with open(inPath) as f:
+        contents = f.read()
+    monsters = json.loads(contents)
     short_monsters = []
     for monster in monsters:
-        short_monsters.append({ "name" : monster.get("Name"), "id" : monster.get("id"), "cr" : monster.get("CR")});
-    print json.dumps(short_monsters);
+        short_monsters.append({ "name" : monster.get("Name"), "id" : monster.get("id"), "cr" : monster.get("CR")})
+    with open(outPath, "w") as f:
+        f.write(json.dumps(short_monsters))
 
 if __name__ == "__main__":
-    path = getPath();
-    processPath(path)
+    inPath, outPath = getPaths()
+    process(inPath, outPath)
+    print "done !"
