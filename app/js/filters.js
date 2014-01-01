20fadc60544713fa179e240e7bfac620e0b10a9d
Nicolas Maquet <nmaquet@gmail.com>
2014-01-01 18:04:55 +0100

    rewrote challengeRating filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,16 +1,12 @@
 'use strict';
 
 angular.module('encounterBuilderFilters', []).filter('challengeRating', function() {
-    return function(input) {
-        var i = 0;
-        var fractions = [ 2, 3, 4, 5, 6, 7, 8];
-
-        for (i in fractions) {
-            if (input === (1 / fractions[i])) {
-                return "1/" + String(fractions[i]);
-            }
+    return function(cr) {
+        if (cr < 1) {
+            return "1/" + String(1/cr);
         }
-
-        return String(input);
+        else {
+            return String(cr);
+        }
     };
 });
