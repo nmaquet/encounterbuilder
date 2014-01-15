b828a4a3c9af95276372ced83fe3d8f6d0c3f1b2
Nicolas Maquet <nmaquet@gmail.com>
2014-01-15 12:14:10 +0100

    added xml2json.js

---
+++ scripts/xml2json.js
@@ -0,0 +1,20 @@
+/**
+ * Created by user on 15/01/14.
+ */
+var parseXml = require('xml2js').parseString;
+var fs = require('fs');
+var inPath = process.argv[2];
+var outPath = process.argv[3];
+
+fs.readFile(inPath, {encoding:'UTF-8'}, function (error, data) {
+    if (error)throw error;
+    parseXml(data.replace("\ufeff", ""), function (error, result) {
+        if (error)throw error;
+
+        fs.writeFile(outPath, JSON.stringify(result), function (error) {
+            if (error)throw error;
+            console.log('Done');
+        });
+    });
+});
+
