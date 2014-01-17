9244e8f8be9d4c960414faece1fc49c46adebba7
Nicolas Maquet <nmaquet@gmail.com>
2014-01-17 12:44:13 +0100

    removed loggings

--- server/monsterRoute.js
+++ server/monsterRoute.js
@@ -5,8 +5,6 @@ module.exports = function (Monster) {
         Monster.find({id: request.params.id}, function (error, monster) {
             if (error)
                 response.send(error);
-            console.log(monster.Name);
-            console.log(monster.SpecialAttacks);
             response.json(monster);
         });
     }
