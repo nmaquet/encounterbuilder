3b3755255e8ba988e9c95b4fdad8af9002992ae2
Nicolas Maquet <nmaquet@gmail.com>
2014-01-20 10:18:09 +0100

    patched E2E tests and fixed test configs

--- test/conf/karma-client-unit.conf.js
+++ test/conf/karma-client-unit.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
         },
         autoWatch: false,
         browsers: ['PhantomJS'], // Firefox, ChromeCanary, Opera, IE
-        reporters: ['junit'],
+        reporters: ['junit', 'progress'],
         junitReporter: {
             outputFile: 'client-unit-test-results.xml',
             suite: ''
--- test/e2e/MonsterListViewSpec.js
+++ test/e2e/MonsterListViewSpec.js
@@ -12,7 +12,7 @@ describe('Monster List View', function () {
 
     it('should display 2 monsters when searching for "wyvern"', function () {
         input('query').enter('wyvern');
-        expect(repeater('.monsters li').count()).toBe(2);
+        expect(repeater('.monsters li').count()).toBe(3);
     });
 
     it('should display one monster when searching for "cube"', function () {
@@ -32,36 +32,40 @@ describe('Monster List View', function () {
     it('should sort by challenge rating when order is challenge rating', function () {
         input('query').enter('goblin');
         select('orderProp').option('cr');
-        expect(repeater('.monsters li').count()).toBe(4);
+        expect(repeater('.monsters li').count()).toBe(5);
         expect(element('.monsters li:nth-child(1) a').text()).toBe("Goblin");
         expect(element('.monsters li:nth-child(2) a').text()).toBe("Hobgoblin");
-        expect(element('.monsters li:nth-child(3) a').text()).toBe("Goblin Dog");
-        expect(element('.monsters li:nth-child(4) a').text()).toBe("Goblin Snake");
+        expect(element('.monsters li:nth-child(3) a').text()).toBe("Monkey Goblin");
+        expect(element('.monsters li:nth-child(4) a').text()).toBe("Goblin Dog");
+        expect(element('.monsters li:nth-child(5) a').text()).toBe("Goblin Snake");
     });
 
     it('should sort by name when order is name', function () {
         input('query').enter('goblin');
         select('orderProp').option('name');
-        expect(repeater('.monsters li').count()).toBe(4);
+        expect(repeater('.monsters li').count()).toBe(5);
         expect(element('.monsters li:nth-child(1) a').text()).toBe("Goblin");
         expect(element('.monsters li:nth-child(2) a').text()).toBe("Goblin Dog");
         expect(element('.monsters li:nth-child(3) a').text()).toBe("Goblin Snake");
         expect(element('.monsters li:nth-child(4) a').text()).toBe("Hobgoblin");
+        expect(element('.monsters li:nth-child(5) a').text()).toBe("Monkey Goblin");
     });
 
     it('should make a list of anchors to the Monster description page', function() {
         input('query').enter('wyvern');
         expect(element('.monsters li:nth-child(1) a').attr("href")).toBe("#/monsters/wyvern");
         expect(element('.monsters li:nth-child(2) a').attr("href")).toBe("#/monsters/aashaq's-wyvern");
+        expect(element('.monsters li:nth-child(3) a').attr("href")).toBe("#/monsters/mythic-wyvern");
     });
 
     it('should display the challenge rating of each monster', function() {
         input('query').enter('goblin');
         select('orderProp').option('cr');
-        expect(repeater('.monsters li').count()).toBe(4);
+        expect(repeater('.monsters li').count()).toBe(5);
         expect(element('.monsters li:nth-child(1) p').text()).toBe("CR : 1/3");
         expect(element('.monsters li:nth-child(2) p').text()).toBe("CR : 1/2");
-        expect(element('.monsters li:nth-child(3) p').text()).toBe("CR : 1");
+        expect(element('.monsters li:nth-child(3) p').text()).toBe("CR : 1/2");
+        expect(element('.monsters li:nth-child(4) p').text()).toBe("CR : 1");
         expect(element('.monsters li:nth-child(4) p').text()).toBe("CR : 1");
     });
 });
