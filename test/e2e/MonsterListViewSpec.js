00629778b97677269fad00c062d9e8ae33e1fa85
Nicolas Maquet <nmaquet@gmail.com>
2014-01-22 12:18:54 +0100

    fixed e2e tests for 3 column view

--- test/e2e/MonsterListViewSpec.js
+++ test/e2e/MonsterListViewSpec.js
@@ -6,18 +6,23 @@ describe('Monster List View', function () {
         browser().navigateTo('/');
     });
 
-    it('should initially display 50 monsters', function () {
-        expect(repeater('.monsters li').count()).toBe(50);
+    function inputNameSubstring(text) {
+        input('nameSubstring').enter(text);
+        sleep(0.5); /* wait for setTimeout to trigger */
+    }
+
+    it('should initially display 50 monsters + 1 header row', function () {
+        expect(repeater('.monsters tr').count()).toBe(50+1);
     });
 
-    it('should display 2 monsters when searching for "wyvern"', function () {
-        input('nameSubstring').enter('wyvern');
-        expect(repeater('.monsters li').count()).toBe(3);
+    it('should display 3 monsters when searching for "wyvern"', function () {
+        inputNameSubstring('wyvern');
+        expect(repeater('.monsters tr').count()).toBe(3+1);
     });
 
     it('should display one monster when searching for "cube"', function () {
-        input('nameSubstring').enter('cube');
-        expect(repeater('.monsters li').count()).toBe(1);
+        inputNameSubstring('cube');
+        expect(repeater('.monsters tr').count()).toBe(1+1);
     });
 
     it('should sort by challenge rating by default', function () {
@@ -30,86 +35,79 @@ describe('Monster List View', function () {
     });
 
     it('should sort by challenge rating when order is challenge rating', function () {
-        input('nameSubstring').enter('goblin');
+        inputNameSubstring('goblin');
         select('orderProp').option('cr');
-        expect(repeater('.monsters li').count()).toBe(5);
-        expect(element('.monsters li:nth-child(1) a').text()).toBe("Goblin");
-        expect(element('.monsters li:nth-child(2) a').text()).toBe("Hobgoblin");
-        expect(element('.monsters li:nth-child(3) a').text()).toBe("Monkey Goblin");
-        expect(element('.monsters li:nth-child(4) a').text()).toBe("Goblin Dog");
-        expect(element('.monsters li:nth-child(5) a').text()).toBe("Goblin Snake");
+        expect(repeater('.monsters tr').count()).toBe(5+1);
+        expect(element('.monsters tr:nth-child(1) td:nth-child(1) p').text()).toBe("Goblin");
+        expect(element('.monsters tr:nth-child(2) td:nth-child(1) p').text()).toBe("Hobgoblin");
+        expect(element('.monsters tr:nth-child(3) td:nth-child(1) p').text()).toBe("Monkey Goblin");
+        expect(element('.monsters tr:nth-child(4) td:nth-child(1) p').text()).toBe("Goblin Dog");
+        expect(element('.monsters tr:nth-child(5) td:nth-child(1) p').text()).toBe("Goblin Snake");
     });
 
     it('should sort by name when order is name', function () {
-        input('nameSubstring').enter('goblin');
+        inputNameSubstring('goblin');
         select('orderProp').option('name');
-        expect(repeater('.monsters li').count()).toBe(5);
-        expect(element('.monsters li:nth-child(1) a').text()).toBe("Goblin");
-        expect(element('.monsters li:nth-child(2) a').text()).toBe("Goblin Dog");
-        expect(element('.monsters li:nth-child(3) a').text()).toBe("Goblin Snake");
-        expect(element('.monsters li:nth-child(4) a').text()).toBe("Hobgoblin");
-        expect(element('.monsters li:nth-child(5) a').text()).toBe("Monkey Goblin");
-    });
-
-    it('should make a list of anchors to the Monster description page', function() {
-        input('nameSubstring').enter('wyvern');
-        expect(element('.monsters li:nth-child(1) a').attr("href")).toBe("#/monsters/wyvern");
-        expect(element('.monsters li:nth-child(2) a').attr("href")).toBe("#/monsters/aashaq's-wyvern");
-        expect(element('.monsters li:nth-child(3) a').attr("href")).toBe("#/monsters/mythic-wyvern");
+        expect(repeater('.monsters tr').count()).toBe(5+1);
+        expect(element('.monsters tr:nth-child(1) td:nth-child(1) p').text()).toBe("Goblin");
+        expect(element('.monsters tr:nth-child(2) td:nth-child(1) p').text()).toBe("Goblin Dog");
+        expect(element('.monsters tr:nth-child(3) td:nth-child(1) p').text()).toBe("Goblin Snake");
+        expect(element('.monsters tr:nth-child(4) td:nth-child(1) p').text()).toBe("Hobgoblin");
+        expect(element('.monsters tr:nth-child(5) td:nth-child(1) p').text()).toBe("Monkey Goblin");
     });
 
     it('should display the challenge rating of each monster', function() {
-        input('nameSubstring').enter('goblin');
+        inputNameSubstring('goblin');
         select('orderProp').option('cr');
-        expect(repeater('.monsters li').count()).toBe(5);
-        expect(element('.monsters li:nth-child(1) p').text()).toBe("CR : 1/3");
-        expect(element('.monsters li:nth-child(2) p').text()).toBe("CR : 1/2");
-        expect(element('.monsters li:nth-child(3) p').text()).toBe("CR : 1/2");
-        expect(element('.monsters li:nth-child(4) p').text()).toBe("CR : 1");
-        expect(element('.monsters li:nth-child(4) p').text()).toBe("CR : 1");
+        expect(repeater('.monsters tr').count()).toBe(5+1);
+        expect(element('.monsters tr:nth-child(1) td:nth-child(2) p').text()).toBe("CR : 1/3");
+        expect(element('.monsters tr:nth-child(2) td:nth-child(2) p').text()).toBe("CR : 1/2");
+        expect(element('.monsters tr:nth-child(3) td:nth-child(2) p').text()).toBe("CR : 1/2");
+        expect(element('.monsters tr:nth-child(4) td:nth-child(2) p').text()).toBe("CR : 1");
+        expect(element('.monsters tr:nth-child(5) td:nth-child(2) p').text()).toBe("CR : 1");
     });
 
     it('should  display 17 monsters when searching for "small"', function () {
-        input('nameSubstring').enter('small');
-        expect(repeater('.monsters li').count()).toBe(17);
+        inputNameSubstring('small');
+        expect(repeater('.monsters tr').count()).toBe(17+1);
     });
     
     it('should  display 2 monsters when searching for "small" with type "animal"', function () {
-        input('nameSubstring').enter('small');
+        inputNameSubstring('small');
         select('type').option('animal');
-        expect(repeater('.monsters li').count()).toBe(2);
+        expect(repeater('.monsters tr').count()).toBe(2+1);
     });
 
     it('should display 4 pages and 4 arrows when searching for "dragon"', function () {
-        input('nameSubstring').enter('dragon');
+        inputNameSubstring('dragon');
         expect(repeater('.page li').count()).toBe(8);
     });
 
     it('should display 2 pages and 4 arrows when searching for "elemental"', function () {
-        input('nameSubstring').enter('elemental');
+        inputNameSubstring('elemental');
         expect(repeater('.page li').count()).toBe(6);
     });
 
     it('should display the correct dragon as first dragon of each page', function () {
-        input('nameSubstring').enter('dragon');
-        expect(element('.monsters li:nth-child(1) a').text()).toBe("Dragonfish");
+        inputNameSubstring('dragon');
+        expect(element('.monsters tr:nth-child(1) td:nth-child(1) p').text()).toBe("Dragonfish");
         element("a:contains(2)").click();
-        expect(element('.monsters li:nth-child(1) a').text()).toBe("Juvenile Green Dragon");
+        expect(element('.monsters tr:nth-child(1) td:nth-child(1) p').text()).toBe("Juvenile Green Dragon");
         element("a:contains(3)").click();
-        expect(element('.monsters li:nth-child(1) a').text()).toBe("Adult Forest Dragon");
+        expect(element('.monsters tr:nth-child(1) td:nth-child(1) p').text()).toBe("Adult Forest Dragon");
         element("a:contains(4)").click();
-        expect(element('.monsters li:nth-child(1) a').text()).toBe("Elemental Air Dragon");
+        expect(element('.monsters tr:nth-child(1) td:nth-child(1) p').text()).toBe("Elemental Air Dragon");
     });
 
     it('should have working pagination arrows', function () {
-        input('nameSubstring').enter('dragon');
+        inputNameSubstring('dragon');
         element("a:contains(›)").click();
-        expect(element('.monsters li:nth-child(1) a').text()).toBe("Juvenile Green Dragon");
+        expect(element('.monsters tr:nth-child(1) td:nth-child(1) p').text()).toBe("Juvenile Green Dragon");
         element("a:contains(»)").click();
-        expect(element('.monsters li:nth-child(1) a').text()).toBe("Elemental Air Dragon");
+        expect(element('.monsters tr:nth-child(1) td:nth-child(1) p').text()).toBe("Elemental Air Dragon");
         element("a:contains(‹)").click();
-        expect(element('.monsters li:nth-child(1) a').text()).toBe("Adult Forest Dragon");
+        expect(element('.monsters tr:nth-child(1) td:nth-child(1) p').text()).toBe("Adult Forest Dragon");
         element("a:contains(«)").click();
-        expect(element('.monsters li:nth-child(1) a').text()).toBe("Dragonfish");
+        expect(element('.monsters tr:nth-child(1) td:nth-child(1) p').text()).toBe("Dragonfish");
     });
 });
