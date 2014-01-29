b68ac4c8b841d5f0daedfd01edb41651020fe3de
Nicolas Maquet <nmaquet@gmail.com>
2014-01-29 20:43:32 +0100

    implemented logout (by clicking on username)

--- client/private/js/controllers.js
+++ client/private/js/controllers.js
@@ -27,6 +27,17 @@ DEMONSQUID.encounterBuilderControllers.controller('LoginController', ['$scope', 
     }
 ]);
 
+DEMONSQUID.encounterBuilderControllers.controller('LogoutController', ['$scope', '$rootScope', '$http', '$location',
+    function ($scope, $rootScope, $http, $location) {
+        $scope.logout = function () {
+            $http.get("/logout").success(function () {
+                delete $rootScope.username;
+                $location.path('/login');
+            });
+        };
+    }
+]);
+
 DEMONSQUID.encounterBuilderControllers.controller('MonsterListController', ['$scope', '$timeout', 'monsterService',
     function ($scope, $timeout, monsterService) {
 
