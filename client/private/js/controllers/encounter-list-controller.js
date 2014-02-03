89d6830b4793314251558fcb181001483122e016
Nicolas Maquet <nmaquet@gmail.com>
2014-02-03 17:26:59 +0100

    finished replacing  usage with services

--- client/private/js/controllers/encounter-list-controller.js
+++ client/private/js/controllers/encounter-list-controller.js
@@ -3,6 +3,9 @@
 DEMONSQUID.encounterBuilderControllers.controller('EncounterListController', ['$scope', 'encounterService',
     function ($scope, encounterService) {
 
+        $scope.selectedEncounter = encounterService.encounters[0];
+        $scope.encounters = encounterService.encounters;
+
         $scope.createEncounter = function() {
             var encounter = encounterService.newEncounter();
             encounterService.encounters.push(encounter);
@@ -10,6 +13,7 @@ DEMONSQUID.encounterBuilderControllers.controller('EncounterListController', ['$
         }
 
         $scope.selectEncounter = function(encounter) {
+            $scope.selectedEncounter = encounter;
             encounterService.selectEncounter(encounter);
         }
     }
