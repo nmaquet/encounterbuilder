d37d79b80cffdc65a6e12a4610931a6af024adb9
Nicolas Maquet <nmaquet@gmail.com>
2014-02-03 16:57:55 +0100

    extracted stuff from global controller

--- client/private/js/controllers/encounter-controller.js
+++ client/private/js/controllers/encounter-controller.js
@@ -1,10 +1,24 @@
 "use strict";
 
-DEMONSQUID.encounterBuilderControllers.controller('EncounterController', ['$scope', 'selectedMonsterService',
-    function ($scope, selectedMonsterService) {
+DEMONSQUID.encounterBuilderControllers.controller('EncounterController', ['$scope', '$rootScope', 'selectedMonsterService',
+    function ($scope, $rootScope, selectedMonsterService) {
 
         $scope.selectMonster = function (id) {
             selectedMonsterService.selectMonster(id);
+        }
+
+        $scope.incrementMonster = function (monster) {
+            monster.amount++;
+        }
+
+        $scope.decrementMonster = function (monster) {
+            if (monster.amount > 1) {
+                monster.amount--;
+            }
+        }
+
+        $scope.removeMonster = function (id) {
+            delete $rootScope.selectedEncounter.Monsters[id];
         }
     }
 ]);
--- client/private/js/controllers/global-controller.js
+++ client/private/js/controllers/global-controller.js
@@ -3,19 +3,5 @@
 DEMONSQUID.encounterBuilderControllers.controller('GlobalController', ['$scope', '$rootScope',
     function ($scope, $rootScope) {
 
-        $scope.incrementMonster = function (monster) {
-            monster.amount++;
-        }
-
-        $scope.decrementMonster = function (monster) {
-            if (monster.amount > 1) {
-                monster.amount--;
-            }
-        }
-
-        $scope.removeMonster = function (id) {
-            delete $rootScope.selectedEncounter.Monsters[id];
-        }
-
     }
 ]);
