a143b57fa32d9f9c967bb3ae61f197ca462f3093
Nicolas Maquet <nmaquet@gmail.com>
2014-02-04 16:39:37 +0100

    force page reload at logoff

--- client/private/js/controllers/logout-controller.js
+++ client/private/js/controllers/logout-controller.js
@@ -1,12 +1,13 @@
 "use strict";
 
-DEMONSQUID.encounterBuilderControllers.controller('LogoutController', ['$scope', '$rootScope', '$http', '$location',
-    function ($scope, $rootScope, $http, $location) {
-        $scope.logout = function () {
-            $http.get("/logout").success(function () {
-                delete $rootScope.username;
-                $location.path('/login');
-            });
-        };
-    }
-]);
+DEMONSQUID.encounterBuilderControllers.controller('LogoutController',
+    ['$scope', '$rootScope', '$http', '$location', '$route', '$timeout',
+        function ($scope, $rootScope, $http, $location, $route, $timeout) {
+            $scope.logout = function () {
+                $http.get("/logout").success(function () {
+                    delete $rootScope.username;
+                    window.location.reload();
+                });
+            };
+        }
+    ]);
