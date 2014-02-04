707e423ba7b9ed55eb89dcbdea1e6744cd90acd7
Nicolas Maquet <nmaquet@gmail.com>
2014-02-04 22:41:11 +0100

    removed auto-creation of untitled encounter & added auto-increment in new encounter creation

--- client/private/js/controllers/encounter-list-controller.js
+++ client/private/js/controllers/encounter-list-controller.js
@@ -7,7 +7,19 @@ DEMONSQUID.encounterBuilderControllers.controller('EncounterListController',
             $scope.encounters = encounterService.encounters;
 
             $scope.createEncounter = function () {
-                var encounter = { Name: "Untitled", CR: "0", Monsters: {}};
+                function exists(name) {
+                    for (var encounter in encounterService.encounters) {
+                        if (encounterService.encounters[encounter].Name === name) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+                var i = 0, encounter;
+                do {
+                    encounter = { Name: "Untitled #" + i, CR: "0", Monsters: {}};
+                    ++i;
+                } while(exists(encounter.Name));
                 selectedEncounterService.selectedEncounter(encounter);
                 encounterService.encounters.push(encounter);
                 encounterService.upsert(encounter);
--- server/userDataRoute.js
+++ server/userDataRoute.js
@@ -5,21 +5,11 @@ module.exports = function (Encounter) {
     return function (request, response) {
         if (request.session && request.session.user) {
 
-            var username =  request.session.user.username;
+            var username = request.session.user.username;
 
-            Encounter.find({Username:username}, function (error, Encounters){
+            Encounter.find({Username: username}, function (error, Encounters) {
                 if (error)
                     response.send(error);
-
-                console.log(Encounters);
-
-                if (Encounters.length === 0) {
-                    console.log("pushing");
-                    Encounters.push({ Name: "Untitled", CR: "0", Monsters: {}});
-                } else{
-                    console.log("not pushing");
-                }
-
                 var userData = {
                     user: {
                         username: username
