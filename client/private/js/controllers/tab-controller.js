347cd694aab328157cd1723fe25f8511d8efcdc4
Nicolas Maquet <nmaquet@gmail.com>
2014-02-05 15:03:55 +0100

    replaced monster & item buttons by a bootstrap tab

--- client/private/js/controllers/tab-controller.js
+++ client/private/js/controllers/tab-controller.js
@@ -4,7 +4,16 @@ DEMONSQUID.encounterBuilderControllers.controller('TabController', ['$scope',
     function ($scope) {
         $scope.selectedTab = 'monsters';
         $scope.showTab = function(tab){
-            $scope.selectedTab =tab;
+            $scope.selectedTab = tab;
         }
+        $('#monstersTab').click(function (e) {
+            e.preventDefault();
+            $(this).tab('show');
+        });
+        $('#itemsTab').click(function (e) {
+            e.preventDefault();
+            $(this).tab('show');
+        })
+        $('#monstersTab').tab('show');
     }
 ]);
