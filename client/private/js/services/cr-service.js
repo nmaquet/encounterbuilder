56027a694930b72b6006e70e46dba49ac240cccd
Nicolas Maquet <nmaquet@gmail.com>
2014-02-11 13:33:22 +0100

    small refactoring of cr-service.js

--- client/private/js/services/cr-service.js
+++ client/private/js/services/cr-service.js
@@ -2,26 +2,30 @@
 
 DEMONSQUID.encounterBuilderServices.factory('crService', [
     function () {
-
-        var crToXp = {1: 400};
-        var xpToCr = {50: 0.125, 65: 1 / 6, 100: 1 / 4, 135: 1 / 3, 200: 0.5, 400: 1};
-        for (var i = 2; i <= 100; i++) {
-            var xp = null;
-            if (i % 2 === 0) {
-                xp = crToXp[i - 1] * (3 / 2);
+        function buildXpToCRTable() {
+            var crToXp = {1: 400};
+            var xpToCr = {50: 0.125, 65: 1 / 6, 100: 1 / 4, 135: 1 / 3, 200: 0.5, 400: 1};
+            for (var i = 2; i <= 100; i++) {
+                var xp = null;
+                if (i % 2 === 0) {
+                    xp = crToXp[i - 1] * (3 / 2);
+                }
+                else {
+                    xp = crToXp[i - 1] * (4 / 3);
+                }
+                crToXp[i] = xp;
+                xpToCr[xp] = i;
             }
-            else {
-                xp = crToXp[i - 1] * (4 / 3);
-            }
-            crToXp[i] = xp;
-            xpToCr[xp] = i;
+            return xpToCr;
         }
+
+        var xpToCr = buildXpToCRTable();
 
         var service = {};
 
         service.calculateCR = function (encounter) {
             var totalXP = Number(encounter.xp);
-            if (totalXP === 0){
+            if (totalXP === 0) {
                 return 0;
             }
             if (xpToCr[totalXP]) {
