0f6ff2d2c9a265cf96a070c6e96165f3b94a0aa6
Nicolas Maquet <nmaquet@gmail.com>
2014-02-19 16:14:40 +0100

    added tables for melee special abilites

--- scripts/enchant_weapons.js
+++ scripts/enchant_weapons.js
@@ -5,52 +5,95 @@ var fs = require('fs');
 var weapons = require(__dirname + "/../data/items/weapons.json");
 var clone = require(__dirname + "/../server/clone.js")().clone;
 
-var meleeSpecialAbilitiesPlusOne = [
-    {name: 'Impervious', flatprice: 3000, cl: 0},
-    {name: 'Glamered', flatprice: 4000, cl: 0},
-    {name: 'Allying', cl: 5},
-    {name: 'Bane', cl: 8},
-    {name: 'Benevolent', cl: 5},
-    {name: 'Called', cl: 9},
-    {name: 'Conductive', cl: 8},
-    {name: 'Corrosive', cl: 10},
-    {name: 'Countering', cl: 5},
-    {name: 'Courageous', cl: 3},
-    {name: 'Cruel', cl: 5},
-    {name: 'Cunning', cl: 6},
-    {name: 'Deadly', cl: 5},
-    {name: 'Defending', cl: 8},
-    {name: 'Dispelling', cl: 10},
-    {name: 'Flaming', cl: 10},
-    {name: 'Frost', cl: 8},
-    {name: 'Furious', cl: 8},
-    {name: 'Ghost touch', cl: 9},
-    {name: 'Grayflame', cl: 6},
-    {name: 'Grounding', cl: 5},
-    {name: 'Guardian', cl: 8},
-    {name: 'Heartseeker', cl: 7},
-    {name: 'Huntsman', cl: 7},
-    {name: 'Jurist', cl: 4},
-    {name: 'Keen', cl: 10},
-    {name: 'Ki focus', cl: 8},
-    {name: 'Limning', cl: 5},
-    {name: 'Menacing', cl: 10},
-    {name: 'Merciful', cl: 5},
-    {name: 'Mighty cleaving', cl: 8},
-    {name: 'Mimetic', cl: 5},
-    {name: 'Neutralizing', cl: 5},
-    {name: 'Ominous', cl: 5},
-    {name: 'Planar', cl: 9},
-    {name: 'Quenching', cl: 5},
-    {name: 'Seaborne', cl: 7},
-    {name: 'Shock', cl: 8},
-    {name: 'Spell storing', cl: 12},
-    {name: 'Thawing', cl: 5},
-    {name: 'Throwing', cl: 5},
-    {name: 'Thundering', cl: 5},
-    {name: 'Valiant', cl: 5},
-    {name: 'Vicious', cl: 9}
-];
+var meleeSpecialAbilities = {
+    1: [
+        {name: 'Impervious', flatprice: 3000, cl: 0},
+        {name: 'Glamered', flatprice: 4000, cl: 0},
+        {name: 'Allying', cl: 5},
+        {name: 'Bane', cl: 8},
+        {name: 'Benevolent', cl: 5},
+        {name: 'Called', cl: 9},
+        {name: 'Conductive', cl: 8},
+        {name: 'Corrosive', cl: 10},
+        {name: 'Countering', cl: 5},
+        {name: 'Courageous', cl: 3},
+        {name: 'Cruel', cl: 5},
+        {name: 'Cunning', cl: 6},
+        {name: 'Deadly', cl: 5},
+        {name: 'Defending', cl: 8},
+        {name: 'Dispelling', cl: 10},
+        {name: 'Flaming', cl: 10},
+        {name: 'Frost', cl: 8},
+        {name: 'Furious', cl: 8},
+        {name: 'Ghost touch', cl: 9},
+        {name: 'Grayflame', cl: 6},
+        {name: 'Grounding', cl: 5},
+        {name: 'Guardian', cl: 8},
+        {name: 'Heartseeker', cl: 7},
+        {name: 'Huntsman', cl: 7},
+        {name: 'Jurist', cl: 4},
+        {name: 'Keen', cl: 10},
+        {name: 'Ki focus', cl: 8},
+        {name: 'Limning', cl: 5},
+        {name: 'Menacing', cl: 10},
+        {name: 'Merciful', cl: 5},
+        {name: 'Mighty cleaving', cl: 8},
+        {name: 'Mimetic', cl: 5},
+        {name: 'Neutralizing', cl: 5},
+        {name: 'Ominous', cl: 5},
+        {name: 'Planar', cl: 9},
+        {name: 'Quenching', cl: 5},
+        {name: 'Seaborne', cl: 7},
+        {name: 'Shock', cl: 8},
+        {name: 'Spell storing', cl: 12},
+        {name: 'Thawing', cl: 5},
+        {name: 'Throwing', cl: 5},
+        {name: 'Thundering', cl: 5},
+        {name: 'Valiant', cl: 5},
+        {name: 'Vicious', cl: 9}
+    ],
+    2: [
+        {name: "Advancing", cl: 5},
+        {name: "Anarchic", cl: 7},
+        {name: "Anchoring", cl: 10},
+        {name: "Axiomatic", cl: 7},
+        {name: "Corrosive burst", cl: 12},
+        {name: "Defiant", cl: 10},
+        {name: "Dispelling burst", cl: 12},
+        {name: "Disruption", cl: 14},
+        {name: "Flaming burst", cl: 12},
+        {name: "Furyborn", cl: 7},
+        {name: "Glorious", cl: 5},
+        {name: "Holy", cl: 7},
+        {name: "Icy burst", cl: 10},
+        {name: "Igniting", cl: 12},
+        {name: "Impact", cl: 9},
+        {name: "Invigorating", cl: 5},
+        {name: "Ki intensifying", cl: 12},
+        {name: "Lifesurge", cl: 8},
+        {name: "Negating", cl: 5},
+        {name: "Phase locking", cl: 7},
+        {name: "Shocking burst", cl: 10},
+        {name: "Stalking", cl: 10},
+        {name: "Unholy", cl: 7},
+        {name: "Wounding", cl: 10}
+    ],
+    3: [
+        {name: "Nullifying", cl: 12},
+        {name: "Repositioning", cl: 10},
+        {name: "Speed", cl: 7},
+        {name: "Spellstealing", cl: 13}
+    ],
+    4: [
+        {name: "Brilliant energy", cl: 16},
+        {name: "Dancing", cl: 15}
+    ],
+    5: [
+        {name: "Vorpal", cl: 18},
+        {name: "Transformative", cl: 10, flatprice: 10000},
+        {name: "Dueling", cl: 5, flatprice: 14000}
+    ]
+};
 
 function idify(string) {
     return string.toLowerCase().replace(" ", "-");
@@ -75,8 +118,8 @@ for (var i in weapons) {
     var weapon = weapons[i];
     if (weapon.WeaponType !== 'ranged' && weapon.WeaponType !== 'ammunition' && weapon.Mwk === false) {
         for (var weaponBonus = 1; weaponBonus <= 5; weaponBonus++) {
-            for (var j in meleeSpecialAbilitiesPlusOne) {
-                var ability = meleeSpecialAbilitiesPlusOne[j];
+            for (var j in meleeSpecialAbilities[1]) {
+                var ability = meleeSpecialAbilities[1][j];
                 var enchantedWeapon = clone(weapon);
                 enchantedWeapon.Mwk = true;
                 enchantedWeapon.Enchanted = true;
