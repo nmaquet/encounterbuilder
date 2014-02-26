f5d4769748d5c815e4a5c6ffebacad55ce789882
Nicolas Maquet <nmaquet@gmail.com>
2014-02-26 16:35:04 +0100

    generated rods without variants + better idify

---
+++ scripts/parse_rods_details.js
@@ -0,0 +1,97 @@
+"use strict";
+
+var fs = require('fs');
+var cheerio = require('cheerio');
+var expect = require('chai').expect;
+var idify = require('../server/idify')().idify;
+
+var html = fs.readFileSync(__dirname + '/../data/contrib/rod_descriptions_prd.html');
+
+var $ = cheerio.load(html);
+
+var elementWithID = function (index) {
+    return this.attr("id") !== undefined;
+};
+
+var rods = [];
+
+
+
+var processParagraphWithID = function (index) {
+    var name = [this.text()];
+    console.log("parsing "  +name );
+
+    var firstParagraph = this.next();
+    expect(firstParagraph.hasClass("stat-block-1")).to.be.true;
+    var priceAuraCLWeight = firstParagraph.text().split(";");
+    var price = [/Price (.*)/.exec(priceAuraCLWeight[0])[1]];
+    var aura = /Aura (.*)/.exec(priceAuraCLWeight[1])[1];
+    var cl = /CL (\d*)/.exec(priceAuraCLWeight[2])[1];
+    var weight = /Weight (.*)/.exec(priceAuraCLWeight[3])[1];
+    var cost;
+    var secondParagraph = firstParagraph.next();
+    var firstDescriptionParagraph;
+
+    if (secondParagraph.hasClass('stat-block-1')) {
+        firstDescriptionParagraph = secondParagraph.next();
+        return  console.log("FIXME ignoring variant for: " + name);
+    } else {
+        firstDescriptionParagraph = secondParagraph;
+        expect(firstDescriptionParagraph.hasClass("stat-block")).to.be.true;
+    }
+
+    var description = "";
+
+    var p = firstDescriptionParagraph;
+    while (!p.hasClass("stat-block-breaker")) {
+        description += p.html();
+        p = p.next();
+    }
+
+    var firstCostParagraph = p.next();
+    expect(firstCostParagraph.hasClass("stat-block-1")).to.be.true;
+
+    cost = cost || [/Cost (.*)/.exec(firstCostParagraph.text())[1]];
+
+    var firstRequirementsParagraph;
+    if (!firstCostParagraph.next().hasClass("stat-block-1")) {
+        firstRequirementsParagraph = firstCostParagraph.next();
+    }
+    else{
+        return  console.log("FIXME ignoring variant for: " + name);
+        firstRequirementsParagraph = firstCostParagraph.next().next();
+    }
+    expect(firstRequirementsParagraph.hasClass("stat-block")).to.be.true;
+
+    var requirements = firstRequirementsParagraph.text();
+    for (var i in name) {
+        var rod = {
+            "CL": Number(cl),
+            "Group": "Rod",
+            "Name": name[i],
+            "Price": parsePrice(price[i]),
+            "PriceUnit": "gp",
+            Aura: aura,
+            Weight: weight,
+            Cost: parsePrice(cost[i]),
+            CostUnit: "gp",
+            Requirements: requirements,
+            Description: description,
+            Source:"Ultimate Equipment",
+            "id": idify(name[i])
+        }
+        rods.push(rod);
+    }
+};
+
+function parsePrice(text) {
+    if (String(text).trim().toLowerCase() === "varies") {
+        return;
+    }
+    else {
+        return Number(text.split(" ")[0].replace(",", ""));
+    }
+}
+
+$("p").filter(elementWithID).each(processParagraphWithID);
+fs.writeFileSync(__dirname + "/../data/items/rods.json", JSON.stringify(rods, null, 4));
--- server/idify.js
+++ server/idify.js
@@ -3,7 +3,19 @@
 module.exports = function () {
     return {
         idify: function (string) {
-            return string.toLowerCase().replace(" ", "-");
+
+            var alphaNumeric = /^[a-z0-9]$/i;
+            var id = "";
+            for (var i in string) {
+                var char = string[i];
+                if (alphaNumeric.test(char)) {
+                    id += char.toLowerCase();
+                }
+                else if (id[id.length - 1] !== "-") {
+                    id += "-";
+                }
+            }
+            return id;
         }
     };
 };
