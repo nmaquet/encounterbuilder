f098f7c5c2df9a1380bc6257e1b91b5c1a50aa1a
Nicolas Maquet <nmaquet@gmail.com>
2014-02-27 14:55:22 +0100

    parsed rods tables

---
+++ scripts/rods_tables.py
@@ -0,0 +1,113 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+
+items = {}
+
+items["lesser_medium"] = """01	Metamagic, merciful, minor	1,500 gp
+02-34	Metamagic (+1 spell level), minor	3,000 gp
+35-49	Immovable rod	5,000 gp
+50-59	Rod of thunderous force	5,400 gp
+60	Metamagic, merciful, normal	5,500 gp
+61	Rod of ice	8,500 gp
+62-71	Metamagic (+2 spell level), minor	9,000 gp
+72-81	Rod of metal and mineral detection	10,500 gp
+82-86	Metamagic (+1 spell level), normal	11,000 gp
+87-100	Rod of cancellation	11,000 gp"""
+items["greater_medium"] = """01-03	Conduit rod	12,000 gp
+04-06	Grounding rod	12,000 gp
+07-10	Rod of the wayang	12,000 gp
+11-18	Rod of wonder	12,000 gp
+19	Metamagic, merciful, greater	12,250 gp
+20-29	Rod of the python	13,000 gp
+30-33	Trap-stealer's rod	13,500 gp
+34-55	Metamagic (+3 spell level), minor	14,000 gp
+56-59	Rod of balance	15,000 gp
+60-63	Rod of escape	15,000 gp
+64-72	Rod of flame extinguishing	15,000 gp
+73-76	Rod of ruin	16,000 gp
+77-80	Sapling rod	16,650 gp
+81-86	Rod of beguiling	18,000 gp
+87-90	Rod of nettles	18,000 gp
+91-100	Rod of the viper	19,000 gp"""
+items["lesser_major"] = """01-04	Suzerain scepter	20,000 gp
+05-06	Fiery nimbus rod	22,305 gp
+07-13	Rod of enemy detection	23,500 gp
+14-27	Metamagic (+1 spell level), greater	24,500 gp
+28-36	Rod of splendor	25,000 gp
+37-44	Rod of withering	25,000 gp
+45-50	Earthbind rod	26,500 gp
+51-56	Rod of the aboleth	29,000 gp
+57-63	Liberator's rod	30,000 gp
+64-70	Metamagic (+2 spell level), normal	32,500 gp
+71-79	Rod of thunder and lightning	33,000 gp
+80-83	Metamagic, quicken, minor	35,000 gp
+84-94	Rod of negation	37,000 gp
+95-100	Rod of steadfast resolve	38,305 gp"""
+items["greater_major"] = """01-08	Rod of absorption	50,000 gp
+09-11	Rod of flailing	50,000 gp
+12-26	Metamagic (+3 spell level), normal	54,000 gp
+27-28	Rod of rulership	60,000 gp
+29-30	Rod of security	61,000 gp
+31-32	Rod of shadows	64,305 gp
+33-34	Rod of mind mastery	67,000 gp
+35-37	Rod of lordly might	70,000 gp
+38-57	Metamagic (+2 spell level), greater	73,000 gp
+58	Scepter of heaven	74,000 gp
+59-68	Metamagic, quicken, normal	75,500 gp
+69-70	Rod of dwarven might	80,000 gp
+71-75	Rod of alertness	85,000 gp
+76-95	Metamagic (+3 spell level), greater	121,500 gp
+96-100	Metamagic, quicken, greater	170,000 gp"""
+
+def item(name, price):
+	result = {
+		"Name": name,
+		"id": slugify(name),
+		"Price": float(price.replace(',','')),
+		"PriceUnit": "gp"
+	}
+	return result
+
+def slugify(string):
+	res = []
+	for c in string:
+		if c.isalnum():
+			res.append(c.lower())
+		elif res and res[-1] != '-':
+			res.append('-')
+	if res[-1] == "-":
+		res.pop()
+	return "".join(res)
+	
+def parseTable(text, magnitude):
+    table = []
+    upperBounds = []
+    values = []
+    for line in text.splitlines():
+        splitted = line.split()
+        chance = splitted[0]
+        if (all(c.isdigit() for c in chance)):
+            lo_chance = hi_chance = int(chance)
+        else:
+            lo_chance, hi_chance = map(int, chance.split("-"))
+        upperBounds.append(hi_chance)
+        price = splitted[-2]
+        name = " ".join(splitted[1:-2])
+        values.append(item(name, price))
+    return """%s : {
+                  create : function() {
+                       return clone(diceService.rangeIn100(this.chanceTable, this.valueTable));
+                  },
+                  chanceTable : %s,
+                  valueTable : %s
+              }""" % (magnitude, json.dumps(upperBounds[:-1]), json.dumps(values))
+
+if __name__ == "__main__":
+    magnitudes = []
+    for magnitude in items:
+        magnitudes.append(parseTable(items[magnitude], magnitude))
+    string = """var random_staff = { \n %s \n };""" % (",\n".join(magnitudes))
+    with open("../data/items/rods_tables.js", "w") as f:
+        f.write(string)
---
+++ server/loot/rods.js
@@ -0,0 +1,102 @@
+"use strict";
+
+var clone = require('./../clone')().clone;
+
+var diceService;
+
+var random_rods = {
+    lesser_major: {
+        create: function () {
+            return clone(diceService.rangeIn100(this.chanceTable, this.valueTable));
+        },
+        chanceTable: [4, 6, 13, 27, 36, 44, 50, 56, 63, 70, 79, 83, 94],
+        valueTable: [
+            {"Price": 20000.0, "PriceUnit": "gp", "Name": "Suzerain scepter", "id": "suzerain-scepter"},
+            {"Price": 22305.0, "PriceUnit": "gp", "Name": "Fiery nimbus rod", "id": "fiery-nimbus-rod"},
+            {"Price": 23500.0, "PriceUnit": "gp", "Name": "Rod of enemy detection", "id": "rod-of-enemy-detection"},
+            {"Price": 24500.0, "PriceUnit": "gp", "Name": "Metamagic (+1 spell level), greater", "id": "metamagic-1-spell-level-greater"},
+            {"Price": 25000.0, "PriceUnit": "gp", "Name": "Rod of splendor", "id": "rod-of-splendor"},
+            {"Price": 25000.0, "PriceUnit": "gp", "Name": "Rod of withering", "id": "rod-of-withering"},
+            {"Price": 26500.0, "PriceUnit": "gp", "Name": "Earthbind rod", "id": "earthbind-rod"},
+            {"Price": 29000.0, "PriceUnit": "gp", "Name": "Rod of the aboleth", "id": "rod-of-the-aboleth"},
+            {"Price": 30000.0, "PriceUnit": "gp", "Name": "Liberator's rod", "id": "liberator-s-rod"},
+            {"Price": 32500.0, "PriceUnit": "gp", "Name": "Metamagic (+2 spell level), normal", "id": "metamagic-2-spell-level-normal"},
+            {"Price": 33000.0, "PriceUnit": "gp", "Name": "Rod of thunder and lightning", "id": "rod-of-thunder-and-lightning"},
+            {"Price": 35000.0, "PriceUnit": "gp", "Name": "Metamagic, quicken, minor", "id": "metamagic-quicken-minor"},
+            {"Price": 37000.0, "PriceUnit": "gp", "Name": "Rod of negation", "id": "rod-of-negation"},
+            {"Price": 38305.0, "PriceUnit": "gp", "Name": "Rod of steadfast resolve", "id": "rod-of-steadfast-resolve"}
+        ]
+    },
+    greater_medium: {
+        create: function () {
+            return clone(diceService.rangeIn100(this.chanceTable, this.valueTable));
+        },
+        chanceTable: [3, 6, 10, 18, 19, 29, 33, 55, 59, 63, 72, 76, 80, 86, 90],
+        valueTable: [
+            {"Price": 12000.0, "PriceUnit": "gp", "Name": "Conduit rod", "id": "conduit-rod"},
+            {"Price": 12000.0, "PriceUnit": "gp", "Name": "Grounding rod", "id": "grounding-rod"},
+            {"Price": 12000.0, "PriceUnit": "gp", "Name": "Rod of the wayang", "id": "rod-of-the-wayang"},
+            {"Price": 12000.0, "PriceUnit": "gp", "Name": "Rod of wonder", "id": "rod-of-wonder"},
+            {"Price": 12250.0, "PriceUnit": "gp", "Name": "Metamagic, merciful, greater", "id": "metamagic-merciful-greater"},
+            {"Price": 13000.0, "PriceUnit": "gp", "Name": "Rod of the python", "id": "rod-of-the-python"},
+            {"Price": 13500.0, "PriceUnit": "gp", "Name": "Trap-stealer's rod", "id": "trap-stealer-s-rod"},
+            {"Price": 14000.0, "PriceUnit": "gp", "Name": "Metamagic (+3 spell level), minor", "id": "metamagic-3-spell-level-minor"},
+            {"Price": 15000.0, "PriceUnit": "gp", "Name": "Rod of balance", "id": "rod-of-balance"},
+            {"Price": 15000.0, "PriceUnit": "gp", "Name": "Rod of escape", "id": "rod-of-escape"},
+            {"Price": 15000.0, "PriceUnit": "gp", "Name": "Rod of flame extinguishing", "id": "rod-of-flame-extinguishing"},
+            {"Price": 16000.0, "PriceUnit": "gp", "Name": "Rod of ruin", "id": "rod-of-ruin"},
+            {"Price": 16650.0, "PriceUnit": "gp", "Name": "Sapling rod", "id": "sapling-rod"},
+            {"Price": 18000.0, "PriceUnit": "gp", "Name": "Rod of beguiling", "id": "rod-of-beguiling"},
+            {"Price": 18000.0, "PriceUnit": "gp", "Name": "Rod of nettles", "id": "rod-of-nettles"},
+            {"Price": 19000.0, "PriceUnit": "gp", "Name": "Rod of the viper", "id": "rod-of-the-viper"}
+        ]
+    },
+    lesser_medium: {
+        create: function () {
+            return clone(diceService.rangeIn100(this.chanceTable, this.valueTable));
+        },
+        chanceTable: [1, 34, 49, 59, 60, 61, 71, 81, 86],
+        valueTable: [
+            {"Price": 1500.0, "PriceUnit": "gp", "Name": "Metamagic, merciful, minor", "id": "metamagic-merciful-minor"},
+            {"Price": 3000.0, "PriceUnit": "gp", "Name": "Metamagic (+1 spell level), minor", "id": "metamagic-1-spell-level-minor"},
+            {"Price": 5000.0, "PriceUnit": "gp", "Name": "Immovable rod", "id": "immovable-rod"},
+            {"Price": 5400.0, "PriceUnit": "gp", "Name": "Rod of thunderous force", "id": "rod-of-thunderous-force"},
+            {"Price": 5500.0, "PriceUnit": "gp", "Name": "Metamagic, merciful, normal", "id": "metamagic-merciful-normal"},
+            {"Price": 8500.0, "PriceUnit": "gp", "Name": "Rod of ice", "id": "rod-of-ice"},
+            {"Price": 9000.0, "PriceUnit": "gp", "Name": "Metamagic (+2 spell level), minor", "id": "metamagic-2-spell-level-minor"},
+            {"Price": 10500.0, "PriceUnit": "gp", "Name": "Rod of metal and mineral detection", "id": "rod-of-metal-and-mineral-detection"},
+            {"Price": 11000.0, "PriceUnit": "gp", "Name": "Metamagic (+1 spell level), normal", "id": "metamagic-1-spell-level-normal"},
+            {"Price": 11000.0, "PriceUnit": "gp", "Name": "Rod of cancellation", "id": "rod-of-cancellation"}
+        ]
+    },
+    greater_major: {
+        create: function () {
+            return clone(diceService.rangeIn100(this.chanceTable, this.valueTable));
+        },
+        chanceTable: [8, 11, 26, 28, 30, 32, 34, 37, 57, 58, 68, 70, 75, 95],
+        valueTable: [
+            {"Price": 50000.0, "PriceUnit": "gp", "Name": "Rod of absorption", "id": "rod-of-absorption"},
+            {"Price": 50000.0, "PriceUnit": "gp", "Name": "Rod of flailing", "id": "rod-of-flailing"},
+            {"Price": 54000.0, "PriceUnit": "gp", "Name": "Metamagic (+3 spell level), normal", "id": "metamagic-3-spell-level-normal"},
+            {"Price": 60000.0, "PriceUnit": "gp", "Name": "Rod of rulership", "id": "rod-of-rulership"},
+            {"Price": 61000.0, "PriceUnit": "gp", "Name": "Rod of security", "id": "rod-of-security"},
+            {"Price": 64305.0, "PriceUnit": "gp", "Name": "Rod of shadows", "id": "rod-of-shadows"},
+            {"Price": 67000.0, "PriceUnit": "gp", "Name": "Rod of mind mastery", "id": "rod-of-mind-mastery"},
+            {"Price": 70000.0, "PriceUnit": "gp", "Name": "Rod of lordly might", "id": "rod-of-lordly-might"},
+            {"Price": 73000.0, "PriceUnit": "gp", "Name": "Metamagic (+2 spell level), greater", "id": "metamagic-2-spell-level-greater"},
+            {"Price": 74000.0, "PriceUnit": "gp", "Name": "Scepter of heaven", "id": "scepter-of-heaven"},
+            {"Price": 75500.0, "PriceUnit": "gp", "Name": "Metamagic, quicken, normal", "id": "metamagic-quicken-normal"},
+            {"Price": 80000.0, "PriceUnit": "gp", "Name": "Rod of dwarven might", "id": "rod-of-dwarven-might"},
+            {"Price": 85000.0, "PriceUnit": "gp", "Name": "Rod of alertness", "id": "rod-of-alertness"},
+            {"Price": 121500.0, "PriceUnit": "gp", "Name": "Metamagic (+3 spell level), greater", "id": "metamagic-3-spell-level-greater"},
+            {"Price": 170000.0, "PriceUnit": "gp", "Name": "Metamagic, quicken, greater", "id": "metamagic-quicken-greater"}
+        ]
+    }
+};
+
+module.exports = function (_diceService_) {
+    diceService = _diceService_;
+    return {
+        random_rods : random_rods
+    }
+};
