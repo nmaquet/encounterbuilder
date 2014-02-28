81a6ca7631a574f679ab187da478068d83f8f298
Nicolas Maquet <nmaquet@gmail.com>
2014-02-28 17:17:33 +0100

    progress on printing

--- client/private/js/controllers/printable-encounter-controller.js
+++ client/private/js/controllers/printable-encounter-controller.js
@@ -4,23 +4,14 @@ DEMONSQUID.encounterBuilderControllers.controller('PrintableEncounterController'
     ['$scope', '$location', '$timeout', 'selectedEncounterService', 'encounterService', 'monsterService',
         function ($scope, $location, $timeout, selectedEncounterService, encounterService, monsterService) {
 
-            $scope.monstersOdd = [];
-            $scope.monstersEven = [];
+            $scope.monsters = [];
 
-            selectedEncounterService.register(function () {
-                $scope.encounter = selectedEncounterService.selectedEncounter();
-                monsterService.getMultiple(Object.keys($scope.encounter.Monsters), function (error, monsters) {
-                    for (var i in monsters) {
-                        if (i % 2 === 0) {
-                            $scope.monstersOdd.push(monsters[i]);
-                        } else{
-                            $scope.monstersEven.push(monsters[i]);
-                        }
-                    }
-                    $timeout(function () {
-                        window.print();
-                    }, 0);
-                });
+            $scope.encounter = selectedEncounterService.selectedEncounter();
+            monsterService.getMultiple(Object.keys($scope.encounter.Monsters), function (error, monsters) {
+                $scope.monsters = monsters;
+                $timeout(function () {
+                    window.print();
+                }, 0);
             });
 
             $scope.back = function () {
