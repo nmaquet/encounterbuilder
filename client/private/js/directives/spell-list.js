efd8e513408f2a57976ca677d85e9fc474e92a95
Nicolas Maquet <nmaquet@gmail.com>
2014-03-20 11:22:29 +0100

    progress in spellListDirective

--- client/private/js/directives/spell-list.js
+++ client/private/js/directives/spell-list.js
@@ -1,15 +1,16 @@
 'use strict';
 
-DEMONSQUID.encounterBuilderDirectives.directive('spellList',[ '$compile',
+DEMONSQUID.encounterBuilderDirectives.directive('spellList', [ '$compile',
+
     function ($compile) {
         var templateLines = [
             '<span>',
-            '<p><strong>{{title}}</strong> (CL {{CL}})</p>',
+            '<p><strong class="spell-title">{{title}}</strong><span class="spell-cl">(CL {{CL}})</span></p>',
             '<ul>',
             '<li class="spell-list-item" ng-repeat="item in spellListItems">{{item}}</li>',
             '</ul>',
             '</span>'
-        ] ;
+        ];
         return {
             restrict: "E",
             replace: true,
@@ -17,10 +18,32 @@ DEMONSQUID.encounterBuilderDirectives.directive('spellList',[ '$compile',
             scope: {
                 spellString: "@spellString"
             },
-            link:function(scope){
-                scope.title = "Spells Barely Known";
-                scope.CL = "0";
-                scope.spellListItems = ["0 (at will)&emdash;detect magic"];
+            link: function (scope) {
+
+                var titleAndCLEnd = scope.spellString.indexOf(")") + 1;
+                var titleAndCL = scope.spellString.slice(0, titleAndCLEnd);
+                var spellList = scope.spellString.slice(titleAndCLEnd);
+
+                scope.title = /([^\(]*) \(CL ([^\)]+)\)/.exec(titleAndCL)[1].trim();
+                scope.CL = /\(CL ([^\)]+)\)/.exec(titleAndCL)[1].trim();
+                scope.spellListItems = [];
+
+                var match;
+                var nextMatch;
+                var matches = [];
+                var spellLevelRegex = /(0|1st|2nd|3rd|4th|5th|6th|7th|8th|9th)([^-]*)-/g;
+                while (null !== (match = spellLevelRegex.exec(spellList))) {
+                    matches.push(match);
+                }
+                for (var i in matches) {
+                    match = matches[i];
+                    nextMatch = matches[Number(i) + 1];
+                    if (nextMatch) {
+                        scope.spellListItems.push(spellList.slice(match.index, nextMatch.index).trim());
+                    } else {
+                        scope.spellListItems.push(spellList.slice(match.index).trim());
+                    }
+                }
             }
         };
     }]);
--- test/client-unit/spellListDirectiveSpec.js
+++ test/client-unit/spellListDirectiveSpec.js
@@ -1,34 +1,47 @@
 "use strict";
 
-var expect = chai.expect;
+(function () {
+    var expect = chai.expect;
 
-var SPELLS_KNOWN_NOOB = "Spells Barely Known (CL 0) 0 (at will)-detect magic";
-var SPELLS_KNOWN_EGARTHIS = "Spells Known (CL 5th) 2nd (5/day)-bull's strength, false life, scorching ray 1st (7/day)-alarm, burning hands (DC 14), color spray (DC 14), mage armor, magic missile 0 (at will)-detect magic, detect poison, mage hand, message, read magic, touch of fatigue (DC 13)";
+    var SPELLS_KNOWN_NOOB_1 = "Spells Barely Known (CL 0) 0 (at will)-detect magic";
+    var SPELLS_KNOWN_NOOB_2 = "Spells Almost Known (CL 0) 0 (at will)-ghost sounds";
+    var SPELLS_KNOWN_EGARTHIS = "Spells Known (CL 5th) 2nd (5/day)-bull's strength, false life, scorching ray 1st (7/day)-alarm, burning hands (DC 14), color spray (DC 14), mage armor, magic missile 0 (at will)-detect magic, detect poison, mage hand, message, read magic, touch of fatigue (DC 13)";
 
-describe("spellList", function () {
+    describe("spellList", function () {
 
-    var $rootScope;
-    var $compile;
+        var $rootScope;
+        var $compile;
 
-    beforeEach(module("encounterBuilderApp"));
+        beforeEach(module("encounterBuilderApp"));
 
-    beforeEach(inject(function (_$compile_, _$rootScope_) {
-        $rootScope = _$rootScope_;
-        $compile = _$compile_;
-    }))
+        beforeEach(inject(function (_$compile_, _$rootScope_) {
+            $rootScope = _$rootScope_;
+            $compile = _$compile_;
+        }))
 
-    function createElement(spellString) {
-        var element = angular.element("<spell-list spell-string=\"" + spellString + "\"></spell-list>");
-        $compile(element)($rootScope)
-        $rootScope.$digest();
-        return element;
-    }
+        function createElement(spellString) {
+            var element = angular.element("<spell-list spell-string=\"" + spellString + "\"></spell-list>");
+            $compile(element)($rootScope)
+            $rootScope.$digest();
+            return element;
+        }
 
-    it("should produce HTML for nOOb spellcasters", function () {
-        var element = createElement(SPELLS_KNOWN_NOOB);
-        expect(element.find("strong").text()).to.equal("Spells Barely Known");
-        expect(element.find("li").length).to.equal(1);
-        expect(element.find("li").hasClass("spell-list-item")).to.equal(true);
-        expect(element.find("li").text()).to.equal("0 (at will)&emdash;detect magic");
+        it("should produce HTML for nOOb spellcaster 1", function () {
+            var element = createElement(SPELLS_KNOWN_NOOB_1);
+            expect(element.find(".spell-title").text()).to.equal("Spells Barely Known");
+            expect(element.find(".spell-cl").text()).to.equal("(CL 0)");
+            expect(element.find("li").length).to.equal(1);
+            expect(element.find("li").hasClass("spell-list-item")).to.equal(true);
+            //expect(element.find("li").text()).to.equal("0 (at will)&emdash;detect magic");
+        });
+
+        it("should produce HTML for nOOb spellcaster 2", function () {
+            var element = createElement(SPELLS_KNOWN_NOOB_2);
+            expect(element.find(".spell-title").text()).to.equal("Spells Almost Known");
+            expect(element.find(".spell-cl").text()).to.equal("(CL 0)");
+            expect(element.find("li").length).to.equal(1);
+            expect(element.find("li").hasClass("spell-list-item")).to.equal(true);
+            //expect(element.find("li").text()).to.equal("0 (at will)&emdash;ghost sounds");
+        });
     });
-});
+})();
