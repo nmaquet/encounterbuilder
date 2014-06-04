9450c7a03b6bad46cfe2b48f9f32c44ef13d359d
Nicolas Maquet <nmaquet@gmail.com>
2014-06-05 10:02:02 +1200

    fixed peristence problem in side-menu directive

--- client/private/js/directives/slide-menu.js
+++ client/private/js/directives/slide-menu.js
@@ -1,44 +1,53 @@
 'use strict';
 
-DEMONSQUID.encounterBuilderDirectives.directive('slideMenu', ['$document', function ($document) {
+DEMONSQUID.encounterBuilderDirectives.directive('slideMenu', ['$document', 'sidebarService', function ($document, sidebarService) {
     return {
         restrict: "A",
         scope: {
             slide: "=slideMenu"
         },
-        link: function (scope, element) {
-            var body = $($document[0].body);
+        compile: function (element) {
 
-            // Substract the Header height from the Slide-Push Menu
-            var $spMenuContent = element.find(".sp-menu-content");
-            var spMenuHeight = $(window).height() - $(".navbar-wrapper").height();
+            if (element.hasClass("sp-menu-left") && sidebarService.leftSidebarOpened) {
+                element.addClass("sp-menu-open");
+            }
 
-            scope.$watch("slide", function () {
-                if (scope.slide) {
-                    if (element.hasClass("sp-menu-left")) {
-                        body.addClass("spm-open-left");
-                    } else if (element.hasClass("sp-menu-right")) {
-                        body.addClass("spm-open-right");
+            if (element.hasClass("sp-menu-right") && sidebarService.rightSidebarOpened) {
+                element.addClass("sp-menu-open");
+            }
+
+            return function (scope, element) {
+                var body = $($document[0].body);
+
+                // Substract the Header height from the Slide-Push Menu
+                var $spMenuContent = element.find(".sp-menu-content");
+                var spMenuHeight = $(window).height() - $(".navbar-wrapper").height();
+
+                scope.$watch("slide", function () {
+                    if (scope.slide) {
+                        if (element.hasClass("sp-menu-left")) {
+                            body.addClass("spm-open-left");
+                        } else if (element.hasClass("sp-menu-right")) {
+                            body.addClass("spm-open-right");
+                        }
+                        element.addClass("sp-menu-open");
+                    } else {
+                        if (element.hasClass("sp-menu-left")) {
+                            body.removeClass("spm-open-left");
+                        } else if (element.hasClass("sp-menu-right")) {
+                            body.removeClass("spm-open-right");
+                        }
+                        element.removeClass("sp-menu-open");
                     }
-                    element.addClass("sp-menu-open");
-                } else {
-                    if (element.hasClass("sp-menu-left")) {
-                        body.removeClass("spm-open-left");
-                    } else if (element.hasClass("sp-menu-right")) {
-                        body.removeClass("spm-open-right");
-                    }
-                    element.removeClass("sp-menu-open");
-                }
-                // Force Maximum height on the vertical Slide-Push Menu,
-                // so that it's scrollable indenpendently of the rest of the body
-                $spMenuContent.height(spMenuHeight);
-                $(window).resize(function() {
-                    spMenuHeight = $(window).height() - $(".navbar-wrapper").height();
+                    // Force Maximum height on the vertical Slide-Push Menu,
+                    // so that it's scrollable indenpendently of the rest of the body
                     $spMenuContent.height(spMenuHeight);
+                    $(window).resize(function() {
+                        spMenuHeight = $(window).height() - $(".navbar-wrapper").height();
+                        $spMenuContent.height(spMenuHeight);
+                    });
                 });
-            });
-
-
+            }
         }
     };
 }]);
