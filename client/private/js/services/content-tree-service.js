18fbaef97f8cff19026732571fdc96df5d7a9d52
Nicolas Maquet <nmaquet@gmail.com>
2014-06-12 14:10:47 +1200

    Revert "safegards against missing feat"
    This reverts commit 63b6a9b9a8bd1c052abb0918f2127cce2af3c763.

--- client/private/js/services/content-tree-service.js
+++ client/private/js/services/content-tree-service.js
@@ -22,6 +22,7 @@ DEMONSQUID.encounterBuilderServices.factory('contentTreeService', ['$rootScope',
             .success(function (userData) {
                 if (userData.contentTree) {
                     contentTree = userData.contentTree;
+                    console.log(contentTree);
                 }
                 $rootScope.$emit(LOAD_SUCCESS);
             })
