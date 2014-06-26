8ca85a6887925137078825fa7d262af1e844e96c
Nicolas Maquet <nmaquet@gmail.com>
2014-06-26 16:48:34 +1200

    improved favouriteTree

--- client/private/js/directives/favourite-tree.js
+++ client/private/js/directives/favourite-tree.js
@@ -6,70 +6,29 @@ DEMONSQUID.encounterBuilderDirectives.directive('favouriteTree',
 
             function link(scope, element) {
 
-                function onClick(event, data) {
-                    if (data.node.folder) {
-                        data.node.setExpanded(!data.node.isExpanded());
-                    }
-                    else {
-                        $timeout(function () {
-                            locationService.goToDetails(data.node.data.type, data.node.data.id);
-                        });
-                    }
-
+                function onActivate(event, data) {
+                    $timeout(function () {
+                        locationService.goToDetails(data.node.data.type, data.node.data.id);
+                    });
                 }
 
                 function onExpandOrCollapse(event, data) {
                     favouriteService.treeChanged();
                 }
 
-
                 var tree;
-
 
                 function handleExtraClasses(node) {
                     node.extraClasses = "fancytree-" + node.data.type;
                 }
 
                 function initTree() {
-
                     element.fancytree({
-//                        extensions: ["dnd"],
                         source: favouriteService.favourites(),
-                        click: onClick,
+                        activate: onActivate,
                         expand: onExpandOrCollapse,
                         collapse: onExpandOrCollapse,
-                        dnd: {
-                            preventVoidMoves: true, // Prevent dropping nodes 'before self', etc.
-                            preventRecursiveMoves: true, // Prevent dropping nodes on own descendants
-                            autoExpandMS: 400,
-//                            dragStart: function (node, data) {
-//                                // This function MUST be defined to enable dragging for the tree.
-//                                // Return false to cancel dragging of node.
-//                                //    if( data.originalEvent.shiftKey ) ...
-//                                return true;
-//                            },
-//                            dragEnter: function (node, data) {
-//                                return node.folder || ["before", "after"];
-//                            },
-//                            dragOver: function (node, data) {
-//                            },
-//                            dragLeave: function (node, data) {
-//                            },
-//                            dragStop: function (node, data) {
-//                            },
-//                            dragDrop: function (node, data) {
-//                                // This function MUST be defined to enable dropping of items on the tree.
-//                                // hitMode is 'before', 'after', or 'over'.
-//                                // We could for example move the source to the new target:
-//                                data.otherNode.moveTo(node, data.hitMode);
-//                                //FIXME
-//                                //favouriteService.treeChanged(tree.toDict(removeExtraClasses));
-//                            },
-//                            draggable: {
-//                                zIndex: 1000,
-//                                scroll: false
-//                            }
-                        }
+                        clickFolderMode: 2 /* expand folder on click */
                     });
                     tree = element.fancytree("getTree");
                     favouriteService.setTree(tree);
