7794ce1c18da59b631847cf7b875bc9625fd57ed
Nicolas Maquet <nmaquet@gmail.com>
2014-07-01 14:21:25 +1200

    progress towards having the script work with a file instead of env variables

--- scripts/live/users.js
+++ scripts/live/users.js
@@ -2,10 +2,10 @@
 
 var program = require('commander');
 var read = require('read');
+var fs = require('fs');
 var async = require('async');
 var MongoClient = require('mongodb').MongoClient;
-
-var MONGODB_URL = "mongodb://nicolas:password@localhost:27017/local";
+var mongodbUrls = JSON.parse(fs.readFileSync("./mongodb_urls.json", 'utf8'));
 
 function randomPassword() {
     var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
@@ -20,21 +20,39 @@ function randomPassword() {
 
 function printUser(user) {
     user.hash = '<hidden>';
-    user.salt = '<hidden>'
+    user.salt = '<hidden>';
     console.log(JSON.stringify(user, null, 4));
 }
 
-MongoClient.connect(MONGODB_URL, function (error, db) {
-    var Users = require("../../server/users")(db);
+function connect(callback) {
+    var url;
+    if (program["live"]){
+        console.log("* using LIVE db *");
+        url = mongodbUrls["live"];
+    } else if (program["test"]) {
+        console.log("* using TEST db *");
+        url = mongodbUrls["test"];
+    } else {
+        console.log("* using STAGING db *");
+        url = mongodbUrls["staging"];
+    }
+    MongoClient.connect(url, function (error, db) {
+        var Users = require("../../server/users")(db);
+        callback(Users, db);
+    });
+}
 
-    program
-        .version('0.0.1');
+program
+    .version('0.0.1')
+    .option('-l --live', "use Live DB")
+    .option('-t --test', "use Test DB");
 
-    program
-        .command("list")
-        .description("list all users")
-        .action(function () {
-            Users.toArray(function(error, userArray){
+program
+    .command("list")
+    .description("list all users")
+    .action(function () {
+        connect(function(Users,  db) {
+            Users.toArray(function (error, userArray) {
                 if (error) {
                     console.log("error listing users : " + error.message);
                 } else {
@@ -46,12 +64,14 @@ MongoClient.connect(MONGODB_URL, function (error, db) {
                 db.close();
             });
         });
+    });
 
-    program
-        .command("show <username>")
-        .description("show one user")
-        .action(function (username) {
-            Users.get(username, function(error, user){
+program
+    .command("show <username>")
+    .description("show one user")
+    .action(function (username) {
+        connect(function(Users,  db) {
+            Users.get(username, function (error, user) {
                 if (error) {
                     console.log("error showing user : " + error.message);
                 } else {
@@ -60,13 +80,15 @@ MongoClient.connect(MONGODB_URL, function (error, db) {
                 db.close();
             });
         });
+    });
 
-    program
-        .command("authenticate <username>")
-        .description("test authentication of user")
-        .action(function (username) {
-            read({ prompt: 'Password: ', silent: true }, function(error, password) {
-                Users.authenticate(username, password, function(error, user){
+program
+    .command("auth <username>")
+    .description("test authentication of user")
+    .action(function (username) {
+        connect(function(Users,  db) {
+            read({ prompt: 'Password: ', silent: true }, function (error, password) {
+                Users.authenticate(username, password, function (error, user) {
                     if (error) {
                         console.log("error authenticating user : " + error.message);
                     } else {
@@ -74,18 +96,20 @@ MongoClient.connect(MONGODB_URL, function (error, db) {
                     }
                     db.close();
                 });
-            })
+            });
         });
+    });
 
-    program
-        .command("register")
-        .description("register a new user")
-        .action(function () {
+program
+    .command("register")
+    .description("register a new user")
+    .action(function () {
+        connect(function(Users,  db) {
             async.series([
                 read.bind(null, { prompt: 'Username: ' }),
                 read.bind(null, { prompt: 'Password (leave blank to generate): ' }),
                 read.bind(null, { prompt: 'Email: ' })
-            ], function(error, results){
+            ], function (error, results) {
                 if (error) {
                     console.log("aborted");
                     return db.close();
@@ -101,9 +125,9 @@ MongoClient.connect(MONGODB_URL, function (error, db) {
                     console.log("Generated password : " + user.password);
                 }
                 if (results[2][0]) {
-                    user.email= results[2][0];
+                    user.email = results[2][0];
                 }
-                Users.register(user, function(error, user) {
+                Users.register(user, function (error, user) {
                     if (error) {
                         console.log("error registering user : " + error.message);
                         return db.close();
@@ -113,15 +137,17 @@ MongoClient.connect(MONGODB_URL, function (error, db) {
                 });
             });
         });
+    });
 
-    program
-        .command("update <username>")
-        .description("update a user's information")
-        .action(function (username) {
+program
+    .command("update <username>")
+    .description("update a user's information")
+    .action(function (username) {
+        connect(function(Users,  db) {
             async.series([
                 read.bind(null, { prompt: 'New username (leave blank if unchanged): ' }),
                 read.bind(null, { prompt: 'New email (leave blank if unchanged): ' })
-            ], function(error, results) {
+            ], function (error, results) {
                 if (error) {
                     console.log("aborted");
                     return db.close();
@@ -137,7 +163,7 @@ MongoClient.connect(MONGODB_URL, function (error, db) {
                     console.log("all fields blank... aborting");
                     return db.close();
                 }
-                Users.update(username, fields, function(error, user) {
+                Users.update(username, fields, function (error, user) {
                     if (error) {
                         console.log("error updating user : " + error.message);
                         return db.close();
@@ -147,12 +173,14 @@ MongoClient.connect(MONGODB_URL, function (error, db) {
                 });
             });
         });
+    });
 
-    program
-        .command("passwd <username>")
-        .description("update a user's password")
-        .action(function (username) {
-            read({ prompt: 'New password (leave blank to generate): ', silent: true}, function(error, password) {
+program
+    .command("passwd <username>")
+    .description("update a user's password")
+    .action(function (username) {
+        connect(function(Users,  db) {
+            read({ prompt: 'New password (leave blank to generate): ', silent: true}, function (error, password) {
                 if (error) {
                     console.log("aborted");
                     return db.close();
@@ -161,7 +189,7 @@ MongoClient.connect(MONGODB_URL, function (error, db) {
                     password = randomPassword();
                     console.log("Generated password : " + password);
                 }
-                Users.updatePassword(username, password, function(error) {
+                Users.updatePassword(username, password, function (error) {
                     if (error) {
                         console.log("error updating user password : " + error.message);
                         return db.close();
@@ -171,16 +199,14 @@ MongoClient.connect(MONGODB_URL, function (error, db) {
                 });
             });
         });
+    });
 
-    program
-        .command("remove <username>")
-        .description("remove a user")
-        .action(function (username) {
-            if (error) {
-                console.log("aborted");
-                return db.close();
-            }
-            Users.remove(username, function(error) {
+program
+    .command("remove <username>")
+    .description("remove a user")
+    .action(function (username) {
+        connect(function(Users,  db) {
+            Users.remove(username, function (error) {
                 if (error) {
                     console.log("error removing user: " + error.message);
                     return db.close();
@@ -189,12 +215,12 @@ MongoClient.connect(MONGODB_URL, function (error, db) {
                 db.close();
             });
         });
+    });
 
-    program.parse(process.argv);
+program.parse(process.argv);
 
-    var COMMANDS = ["list", "show", "update", "passwd", "authenticate", "register", "remove"];
+var COMMANDS = ["list", "show", "update", "passwd", "auth", "register", "remove"];
 
-    if (program.args.length === 0 || COMMANDS.indexOf(program.rawArgs[2]) < 0) {
-        program.help();
-    }
-});
+if (program.args.length === 0 || COMMANDS.indexOf(program.rawArgs[2]) < 0) {
+    program.help();
+}
