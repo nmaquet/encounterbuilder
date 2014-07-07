34f41d3214bd15ccf8b22281e5e1fe81a554b6a1
Nicolas Maquet <nmaquet@gmail.com>
2014-07-07 15:22:49 +1200

    tutorial only for desktop

--- client/private/js/controllers/tutorial-controller.js
+++ client/private/js/controllers/tutorial-controller.js
@@ -1,35 +1,46 @@
 "use strict";
 
-DEMONSQUID.encounterBuilderControllers.controller('TutorialController', ['$scope', '$timeout', function($scope, $timeout) {
+DEMONSQUID.encounterBuilderControllers.controller('TutorialController', ['$scope', '$timeout', 'viewportService',
+    function($scope, $timeout, viewportService) {
+        var TEMPLATE = '<div class="popover tutorial-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>';
 
-    var TEMPLATE = '<div class="popover tutorial-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>';
+        if (!viewportService.viewport.lg) {
+            return;
+        }
 
-    var popovers = [];
+        var popovers = [];
 
-    function createPopover(element, placement, text) {
+        function createPopover(element, placement, text) {
 
-        var p = element.popover({
-            title: '',
-            content: text,
-            placement: placement,
-            trigger: 'manual',
-            container: 'body',
-            template: TEMPLATE
+            var p = element.popover({
+                title: '',
+                content: text,
+                placement: placement,
+                trigger: 'manual',
+                container: 'body',
+                template: TEMPLATE
+            });
+            popovers.push(p);
+        }
+
+        $timeout(function() {
+            createPopover($('#create-dropdown'), "right", "Use this dropdown to create new content.");
+            createPopover($('#left-sidebar'), "right", "Organize your content here using binders. Drag & drop to move things around.");
+            createPopover($('#search-tabs'), "right", "Use these tabs to switch between your Library, Favourites, and Database.");
+            createPopover($('#right-sidebar'), "left", "Access the database from anywhere using this sidebar.");
+            createPopover($('.icon-plus').first(), "left", "Use these buttons to add monsters, NPC's, and items to encounters.");
+            createPopover($('#database-tabs'), "left", "Use these tabs to switch between Monsters, NPC's, Items, Spells and Feats.");
+            $(popovers).popover('toggle');
+        }, 1000);
+
+        $scope.$on("$locationChangeStart", function() {
+            $(popovers).popover('destroy');
         });
-        popovers.push(p);
+
+        viewportService.register(function() {
+            if (!viewportService.viewport.lg) {
+                $(popovers).popover('destroy');
+            }
+        });
     }
-
-    $timeout(function() {
-        createPopover($('#create-dropdown'), "right", "Use this dropdown to create new content.");
-        createPopover($('#left-sidebar'), "right", "Organize your content here using binders. Drag & drop to move things around.");
-        createPopover($('#search-tabs'), "right", "Use these tabs to switch between your Library, Favourites, and Database.");
-        createPopover($('#right-sidebar'), "left", "Access the database from anywhere using this sidebar.");
-        createPopover($('.icon-plus').first(), "left", "Use these buttons to add monsters, NPC's, and items to encounters.");
-        createPopover($('#database-tabs'), "left", "Use these tabs to switch between Monsters, NPC's, Items, Spells and Feats.");
-        $(popovers).popover('toggle');
-    }, 1000);
-
-    $scope.$on("$locationChangeStart", function() {
-        $(popovers).popover('destroy');
-    });
-}]);
+]);
