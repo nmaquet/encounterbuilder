78c93168336b947dba7b4b3590e17bf1f5b5b57e
Nicolas Maquet <nmaquet@gmail.com>
2014-07-07 21:44:17 +1200

    first pass at phone tutorial

--- client/private/js/controllers/tutorial-controller.js
+++ client/private/js/controllers/tutorial-controller.js
@@ -1,17 +1,12 @@
 "use strict";
 
-DEMONSQUID.encounterBuilderControllers.controller('TutorialController', ['$scope', '$timeout', 'viewportService',
-    function($scope, $timeout, viewportService) {
+DEMONSQUID.encounterBuilderControllers.controller('TutorialController', ['$scope', '$timeout', 'viewportService', 'sidebarService',
+    function($scope, $timeout, viewportService, sidebarService) {
         var TEMPLATE = '<div class="popover tutorial-popover is-pointer" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>';
-
-        if (!viewportService.viewport.lg) {
-            return;
-        }
 
         var popovers = [];
 
         function createPopover(element, placement, text) {
-
             var p = element.popover({
                 title: '',
                 content: text,
@@ -28,7 +23,7 @@ DEMONSQUID.encounterBuilderControllers.controller('TutorialController', ['$scope
             $("#monstersTab").click();
         });
 
-        $timeout(function() {
+        function createDesktopPopovers() {
             createPopover($('#create-dropdown'), "right", "Use this dropdown to create new content.");
             createPopover($('#left-sidebar'), "right", "Organize your content here using binders. Drag & drop to move things around.");
             createPopover($('#search-tabs'), "right", "Use these tabs to switch between your Library, Favourites, and Database.");
@@ -39,21 +34,58 @@ DEMONSQUID.encounterBuilderControllers.controller('TutorialController', ['$scope
             createPopover($('#example-monster-favourite-btn'), "bottom", "Favourite");
             createPopover($('#example-monster-edit-btn'), "top", "Edit");
             createPopover($('#example-monster-delete-btn'), "right", "Delete");
+        }
+
+        function createPhonePopovers() {
+            createPopover($('#open-sidebar-and-select-db-tab'), "bottom", "Use these to access your Library, Favourites, and Database.");
+            createPopover($('#example-monster-delete-btn'), "bottom", "Use these to Copy, Edit, Favourite, and Delete.");
+            createPopover($('#left-tutorial-popover-anchor'), "right", "Swipe right to access your Library, Favourites, and Database.");
+            createPopover($('#right-tutorial-popover-anchor'), "left", "Swipe left to the Database.");
+        }
+
+        function destroyAndRemovePopovers() {
+            $(popovers).popover('destroy');
+            $('.popover.tutorial-popover').remove();
+        }
+
+        function createPopovers() {
+            if (viewportService.viewport.lg) {
+                createDesktopPopovers();
+            } else if (viewportService.viewport.xs || viewportService.viewport.sm) {
+                createPhonePopovers();
+            }
             $(popovers).popover('toggle');
             $('.popover.tutorial-popover').click(function (event) {
                 $(event.currentTarget).remove();
             });
+        }
+
+        $timeout(function() {
+            createPopovers();
         }, 1000);
 
         $scope.$on("$locationChangeStart", function() {
-            $(popovers).popover('destroy');
-            $('.popover.tutorial-popover').remove();
+            destroyAndRemovePopovers();
         });
 
-        viewportService.register(function() {
-            if (!viewportService.viewport.lg) {
-                $(popovers).popover('destroy');
-            }
-        });
+        if (viewportService.viewport.lg) {
+            $scope.$watch(sidebarService.rightSidebarOpened.get, function (opened) {
+                if (!opened) {
+                    destroyAndRemovePopovers();
+                }
+            });
+        }
+        else if (viewportService.viewport.xs || viewportService.viewport.sm) {
+            $scope.$watch(sidebarService.leftSidebarOpened.get, function (opened) {
+                if (opened) {
+                    destroyAndRemovePopovers();
+                }
+            });
+            $scope.$watch(sidebarService.rightSidebarOpened.get, function (opened) {
+                if (opened) {
+                    destroyAndRemovePopovers();
+                }
+            });
+        }
     }
 ]);
