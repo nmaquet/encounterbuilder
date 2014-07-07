a824d06ca2672124a6ca8bc9b8edc3e162a24180
Nicolas Maquet <nmaquet@gmail.com>
2014-07-07 13:19:00 +1200

    tutorial controller creates popovers

--- client/private/js/controllers/tutorial-controller.js
+++ client/private/js/controllers/tutorial-controller.js
@@ -1,5 +1,32 @@
 "use strict";
 
 DEMONSQUID.encounterBuilderControllers.controller('TutorialController', ['$scope', function($scope) {
-    alert("scope initialized");
+
+    var popovers = [];
+
+    popovers.push($('#create-dropdown').popover({
+       title: '',
+       content: "this is the create dropdown",
+       placement: "right",
+       trigger: 'manual',
+       container: 'body'
+    }));
+
+    popovers.push($('#main-title').popover({
+        title: '',
+        content: "this is the main title",
+        placement: "right",
+        trigger: 'manual',
+        container: 'body'
+    }));
+
+    for (var i in popovers) {
+        popovers[i].popover('toggle');
+    }
+
+    $scope.$on("$locationChangeStart", function() {
+        for (var i in popovers) {
+            popovers[i].popover('destroy');
+        }
+    });
 }]);
