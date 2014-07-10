a19db98bd1c3e031d74cabd14b9f59ad165bd45a
Nicolas Maquet <nmaquet@gmail.com>
2014-07-10 15:13:45 +1200

    added a failures object

--- client/private/js/services/formatter-service.js
+++ client/private/js/services/formatter-service.js
@@ -4,34 +4,34 @@ DEMONSQUID.encounterBuilderServices.factory('formatterService', [
 
     function () {
 
-        function formatUnsignedNumber(monster, parsedMonster, attribute) {
+        function formatUnsignedNumber(monster, parsedMonster, attribute, failures) {
             if (!isNaN(parsedMonster[attribute])) {
                 monster[attribute] = parsedMonster[attribute];
             }
         }
 
-        function formatSignedNumber(monster, parsedMonster, attribute) {
+        function formatSignedNumber(monster, parsedMonster, attribute, failures) {
             if (!isNaN(parsedMonster[attribute])) {
                 var sign = parsedMonster[attribute] >= 0 ? "+" : "-";
                 monster[attribute] = sign + parsedMonster[attribute];
             }
         }
 
-        function formatArmorClass(monster, parsedMonster, attribute) {
+        function formatArmorClass(monster, parsedMonster, attribute, failures) {
             var normal = parsedMonster.normalAC;
             var touch = parsedMonster.touchAC;
             var flatFooted = parsedMonster.flatFootedAC;
             monster.AC = normal + ", touch " + touch + ", flat-footed " + flatFooted;
         }
 
-        function formatSkills(monster, parsedMonster, attribute) {
+        function formatSkills(monster, parsedMonster, attribute, failures) {
             monster.Skills = parsedMonster.Skills.map(function (value) {
                 var sign = (value.mod >= 0) ? "+" : "-";
                 return (value.name + " " + sign + value.mod);
             }).join(", ");
         }
 
-        function formatHitDice(monster, parsedMonster, attribute) {
+        function formatHitDice(monster, parsedMonster, attribute, failures) {
             if (parsedMonster.numberOfHD && parsedMonster.typeOfHD && parsedMonster.hitPointBonus) {
                 var hitDice = parsedMonster.numberOfHD;
                 var dieType = parsedMonster.typeOfHD;
@@ -62,11 +62,13 @@ DEMONSQUID.encounterBuilderServices.factory('formatterService', [
         var service = {};
 
         service.formatMonster = function(monster, parsedMonster) {
+            var failures = {};
             for (var attribute in formatters) {
                 if (!formatters.hasOwnProperty(attribute))
                     continue;
-                formatters[attribute](monster, parsedMonster, attribute);
+                formatters[attribute](monster, parsedMonster, attribute, failures);
             }
+            return failures;
         };
 
         return service;
