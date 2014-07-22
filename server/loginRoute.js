de6304401590d316cf5b91bb7fa356b63328d321
Nicolas Maquet <nmaquet@gmail.com>
2014-07-22 19:04:21 +1200

    added CORS login route

--- server/loginRoute.js
+++ server/loginRoute.js
@@ -1,19 +1,33 @@
 "use strict";
 
 module.exports = function (userService) {
-    return function (request, response) {
-        if (request.host !== "localhost" && request.protocol !== "https") {
-            return response.send(400, "login must done over a secure connection")
-        }
-        userService.authenticate(request.body.username, request.body.password, function (error, user) {
-            if (user) {
-                request.session.regenerate(function () {
-                    request.session.user = user;
-                    response.send(200);
-                });
-            } else {
-                response.send(401, "login failed");
+    return {
+        post: function (request, response) {
+            if (request.host !== "localhost" && request.protocol !== "https") {
+                return response.send(400, "login must done over a secure connection")
             }
-        });
-    }
+            userService.authenticate(request.body.username, request.body.password, function (error, user) {
+                if (user) {
+                    request.session.regenerate(function () {
+                        request.session.user = user;
+                        response.send(200);
+                    });
+                } else {
+                    response.send(401, "login failed");
+                }
+            });
+        },
+        options: function (request, response) {
+            var allowedOrigin;
+            if (request.host === "staging.encounterbuilder.com") {
+                allowedOrigin = "encounterbuilder-staging.herokuapp.com";
+            } else {
+                allowedOrigin = "encounterbuilder-live.herokuapp.com";
+            }
+            response.header('Access-Control-Allow-Origin', allowedOrigin);
+            response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+            response.header('Access-Control-Allow-Headers', 'Content-Type');
+            response.send(200);
+        }
+    };
 };
