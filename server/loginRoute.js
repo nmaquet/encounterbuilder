7069204d4255780f0d2844b3d717a516bdabefe8
Nicolas Maquet <nmaquet@gmail.com>
2014-07-22 23:27:46 +1200

    yet another monkey patch: removed CORS code in /login POST

--- server/loginRoute.js
+++ server/loginRoute.js
@@ -6,15 +6,6 @@ module.exports = function (userService) {
             if (request.host !== "localhost" && request.protocol !== "https") {
                 return response.send(400, "login must done over a secure connection")
             }
-            var allowedOrigin;
-            if (request.host === "encounterbuilder-staging.herokuapp.com") {
-                allowedOrigin = "http://staging.encounterbuilder.com";
-            } else {
-                allowedOrigin = "http://www.encounterbuilder.com";
-            }
-            response.header('Access-Control-Allow-Origin', allowedOrigin);
-            response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-            response.header('Access-Control-Allow-Headers', 'Content-Type');
             userService.authenticate(request.body.username, request.body.password, function (error, user) {
                 if (user) {
                     request.session.regenerate(function () {
