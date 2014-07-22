6c39291e49464c30b0dc620fea0f9e08dcd3b46d
Nicolas Maquet <nmaquet@gmail.com>
2014-07-22 19:21:39 +1200

    added CORS login route

--- server/loginRoute.js
+++ server/loginRoute.js
@@ -6,6 +6,11 @@ module.exports = function (userService) {
             if (request.host !== "localhost" && request.protocol !== "https") {
                 return response.send(400, "login must done over a secure connection")
             }
+            if (request.host === "staging.encounterbuilder.com") {
+                allowedOrigin = "encounterbuilder-staging.herokuapp.com";
+            } else {
+                allowedOrigin = "encounterbuilder-live.herokuapp.com";
+            }
             response.header('Access-Control-Allow-Origin', allowedOrigin);
             response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
             response.header('Access-Control-Allow-Headers', 'Content-Type');
