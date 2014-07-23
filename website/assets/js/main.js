3d0c6fbb125755a3750ec996d9778da8f5128ef9
Nicolas Maquet <nmaquet@gmail.com>
2014-07-24 11:53:22 +1200

    started to adapt the client code for authentication

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -1,87 +1,80 @@
 "use strict";
 
-$('.carousel').carousel({
-    interval: 6000
-});
+(function () {
 
-$("#login-form").submit(function () {
-    var url;
-    var crossdomain = true;
-    if (window.location.host === "localhost:3000" || window.location.host === "localhost.encounterbuilder.com") {
-        url = "http://localhost:3000/login";
-        crossdomain = false;
-    } else if (window.location.host === "staging.encounterbuilder.com") {
-        url = "https://encounterbuilder-staging.herokuapp.com/login";
-    } else {
-        url = "https://encounterbuilder-live.herokuapp.com/login";
-    }
-//    var formData = $("#login-form").serializeArray().reduce(function(obj, item) {
-//        obj[item.name] = item.value;
-//        return obj;
-//    }, {});
-    var formData = $("#login-form").serialize();
-    $.ajax({
-        type: "POST",
-        url: url,
-//        contentType: "application/json",
-        crossDomain: crossdomain,
-        xhrFields: {
-            withCredentials: true
-        },
-        data: formData,
-        success: function (data) {
-            window.location.href = "/app";
-        },
-        error: function (data) {
-            $("#login-failed-alert").removeClass("hidden");
-        }
+    $('.carousel').carousel({
+        interval: 6000
     });
-    return false; // avoid to execute the actual submit of the form.
-});
 
-$('#login').on('shown.bs.modal', function () {
-    $('#username').focus();
-});
+    var HOST_TO_LOGIN_URL = {
+        "localhost:3000": "http://localhost:3000/login",
+        "localhost.encounterbuilder.com": "http://localhost:3000/login",
+        "staging.encounterbuilder.com": "https://encounterbuilder-staging.herokuapp.com/login",
+        "www.encounterbuilder.com": "https://encounterbuilder-live.herokuapp.com/login"
+    };
 
-$(window).load(function () {
-    $.ajax({
-        type: "post",
-        url: '/api/user-data',
-        success: function (userData) {
-            if (userData.user !== undefined) {
-                $('#login-link').addClass('hide');
-                $('#register-link').addClass('hide');
-                $('#logged-in').removeClass('hide');
+    $("#login-form").submit(function () {
+        $.ajax({
+            type: "POST",
+            url: HOST_TO_LOGIN_URL[window.location.host],
+            crossDomain: true,
+            data: $("#login-form").serialize(),
+            success: function (data) {
+                // TODO: store the JWT into session storage
+                window.location.href = "/app";
+            },
+            error: function (data) {
+                $("#login-failed-alert").removeClass("hidden");
             }
+        });
+        return false; // avoid to execute the actual submit of the form.
+    });
+
+    $('#login').on('shown.bs.modal', function () {
+        $('#username').focus();
+    });
+
+    $(window).load(function () {
+        $.ajax({
+            type: "post",
+            url: '/api/user-data',
+            success: function (userData) {
+                if (userData.user !== undefined) {
+                    $('#login-link').addClass('hide');
+                    $('#register-link').addClass('hide');
+                    $('#logged-in').removeClass('hide');
+                }
+            }
+        });
+        var queryStrings = getQueryStrings();
+        if (queryStrings['promptLogin'] === 'true') {
+            $('#login').modal('show');
+        }
+        if ($('#monthlyRadio').prop('checked')) {
+            monthlyRadioClicked();
+        } else {
+            yearlyRadioClicked();
         }
     });
-    var queryStrings = getQueryStrings();
-    if (queryStrings['promptLogin'] === 'true') {
-        $('#login').modal('show');
-    }
-    if ($('#monthlyRadio').prop('checked')) {
-        monthlyRadioClicked();
-    } else {
-        yearlyRadioClicked();
-    }
-});
 
-function getQueryStrings() {
-    var vars = {}, hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for (var i = 0; i < hashes.length; i++) {
-        hash = hashes[i].split('=');
-        vars[hash[0]] = hash[1];
+    function getQueryStrings() {
+        var vars = {}, hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for (var i = 0; i < hashes.length; i++) {
+            hash = hashes[i].split('=');
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
     }
-    return vars;
-}
 
-function monthlyRadioClicked() {
-    $('.yearlyPrice').addClass('hide');
-    $('.monthlyPrice').removeClass('hide');
-}
+    function monthlyRadioClicked() {
+        $('.yearlyPrice').addClass('hide');
+        $('.monthlyPrice').removeClass('hide');
+    }
 
-function yearlyRadioClicked() {
-    $('.monthlyPrice').addClass('hide');
-    $('.yearlyPrice').removeClass('hide');
-}
+    function yearlyRadioClicked() {
+        $('.monthlyPrice').addClass('hide');
+        $('.yearlyPrice').removeClass('hide');
+    }
+
+})();
