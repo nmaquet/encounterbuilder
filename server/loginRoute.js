b91af972c024d4d911198953711f37c23f2873e2
Nicolas Maquet <nmaquet@gmail.com>
2014-07-23 13:42:02 +1200

    yet another attempt at CORS

--- server/loginRoute.js
+++ server/loginRoute.js
@@ -6,6 +6,9 @@ module.exports = function (userService) {
             if (request.host !== "localhost" && request.protocol !== "https") {
                 return response.send(400, "login must done over a secure connection")
             }
+            response.header('Access-Control-Allow-Origin', '*');
+            response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+            response.header('Access-Control-Allow-Headers', 'Content-Type');
             userService.authenticate(request.body.username, request.body.password, function (error, user) {
                 if (user) {
                     request.session.regenerate(function () {
@@ -18,13 +21,13 @@ module.exports = function (userService) {
             });
         },
         options: function (request, response) {
-            var allowedOrigin;
-            if (request.host === "encounterbuilder-staging.herokuapp.com") {
-                allowedOrigin = "http://staging.encounterbuilder.com";
-            } else {
-                allowedOrigin = "http://www.encounterbuilder.com";
-            }
-            response.header('Access-Control-Allow-Origin', allowedOrigin);
+//            var allowedOrigin;
+//            if (request.host === "encounterbuilder-staging.herokuapp.com") {
+//                allowedOrigin = "http://staging.encounterbuilder.com";
+//            } else {
+//                allowedOrigin = "http://www.encounterbuilder.com";
+//            }
+            response.header('Access-Control-Allow-Origin', '*');
             response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
             response.header('Access-Control-Allow-Headers', 'Content-Type');
             response.send(200);
