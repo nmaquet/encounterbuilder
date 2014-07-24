9a85f2c1b77f3c7af476a1f52ab93df3ff344098
Nicolas Maquet <nmaquet@gmail.com>
2014-07-24 12:42:59 +1200

    modified interceptor service to put the token into the Authorization header

--- client/private/js/services/http-interceptor-service.js
+++ client/private/js/services/http-interceptor-service.js
@@ -3,6 +3,13 @@
 DEMONSQUID.encounterBuilderServices.factory('httpInterceptorService', ['$q', '$window',
     function ($q, $window) {
         return {
+            request: function (config) {
+                config.headers = config.headers || {};
+                if ($window.sessionStorage.token) {
+                    config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+                }
+                return config;
+            },
             'responseError': function (rejection) {
                 if (rejection.status === 401) {
                     $window.location.href = "/?promptLogin=true";
