cbe73b909fe09732e964f9ff144d04a077591a42
Nicolas Maquet <nmaquet@gmail.com>
2014-07-31 14:26:18 +1200

    pricing changes

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -1,156 +1,162 @@
 "use strict";
 
-(function () {
+$('.carousel').carousel({
+    interval: 6000
+});
 
-    $('.carousel').carousel({
-        interval: 6000
+var HOST_URL = {
+    "localhost:3000": "http://localhost:3000/",
+    "localhost.encounterbuilder.com": "http://localhost.encounterbuilder.com/",
+    "staging.encounterbuilder.com": "https://encounterbuilder-staging.herokuapp.com/",
+    "www.encounterbuilder.com": "https://encounterbuilder-live.herokuapp.com/"
+};
+
+
+$("#login-form").submit(function () {
+    var rememberme = $('#rememberme').is(':checked');
+    $.ajax({
+        type: "POST",
+        url: HOST_URL[window.location.host] + "login",
+        crossDomain: true,
+        data: $("#login-form").serialize(),
+        success: function (data) {
+            if (rememberme){
+                window.localStorage.setItem("token", data.token);
+            }
+            else {
+                window.sessionStorage.setItem("token", data.token);
+            }
+            $("#login-failed-alert").addClass("hidden");
+            window.location.href = "/app";
+        },
+        error: function (error) {
+            if (error.status === 403) {
+                window.sessionStorage.removeItem("token");
+                window.localStorage.removeItem("token");
+                $("#login-failed-email-notvalidated").removeClass("hidden");
+            }
+            else {
+                window.sessionStorage.removeItem("token");
+                window.localStorage.removeItem("token");
+                $("#login-failed-alert").removeClass("hidden");
+            }
+        }
     });
+    return false; // avoid to execute the actual submit of the form.
+});
 
-    var HOST_URL = {
-        "localhost:3000": "http://localhost:3000/",
-        "localhost.encounterbuilder.com": "http://localhost.encounterbuilder.com/",
-        "staging.encounterbuilder.com": "https://encounterbuilder-staging.herokuapp.com/",
-        "www.encounterbuilder.com": "https://encounterbuilder-live.herokuapp.com/"
-    };
+$("#register-form").submit(function () {
+    $.ajax({
+        type: "POST",
+        url: HOST_URL[window.location.host] + "register",
+        crossDomain: true,
+        data: $("#register-form").serialize(),
+        success: function (data) {
+            if (data.error) {
+                if (data.error === 'USERNAME_ALREADY_EXISTS') {
+                    $("#register-failed-user").removeClass("hidden");
+                }
+                else if (data.error === 'EMAIL_ALREADY_EXISTS') {
+                    $("#register-failed-email").removeClass("hidden");
+                }
+                else {
+                    $("#register-failed-alert").removeClass("hidden");
+                }
 
+            }
+            else {
+                $("#register-failed-alert").addClass("hidden");
+                $("#register-failed-user").addClass("hidden");
+                $("#register-failed-email").addClass("hidden");
+                $('#register').modal('hide');
+                $('#register-success').modal('show');
+            }
+        },
+        error: function (data) {
+            $("#register-failed-alert").removeClass("hidden");
+        }
+    });
+    return false; // avoid to execute the actual submit of the form.
+});
 
-    $("#login-form").submit(function () {
-        var rememberme = $('#rememberme').is(':checked');
+$('#login').on('shown.bs.modal', function () {
+    $("#login-failed-alert").addClass("hidden");
+    $("#login-failed-email-notvalidated").addClass("hidden");
+    $("#login-email-validated").addClass("hidden");
+    $('#username').focus();
+});
+
+$('#register').on('shown.bs.modal', function () {
+    $("#register-failed-alert").addClass("hidden");
+    $("#register-failed-user").addClass("hidden");
+    $("#register-failed-email").addClass("hidden");
+});
+
+$(function () {
+    $('a.register-btn').click(function () {
+        $('#login .close').click();
+        $('#register').modal('show');
+    });
+});
+
+$(window).load(function () {
+    var token = window.sessionStorage.getItem("token")||window.localStorage.getItem("token");
+    if (token) {
         $.ajax({
-            type: "POST",
-            url: HOST_URL[window.location.host] + "login",
-            crossDomain: true,
-            data: $("#login-form").serialize(),
-            success: function (data) {
-                if (rememberme){
-                    window.localStorage.setItem("token", data.token);
-                }
-                else {
-                    window.sessionStorage.setItem("token", data.token);
-                }
-                $("#login-failed-alert").addClass("hidden");
-                window.location.href = "/app";
+            type: "post",
+            url: '/api/user-data',
+            headers: {
+                "Authorization": "Bearer " + token
             },
-            error: function (error) {
-                if (error.status === 403) {
-                    window.sessionStorage.removeItem("token");
-                    window.localStorage.removeItem("token");
-                    $("#login-failed-email-notvalidated").removeClass("hidden");
-                }
-                else {
-                    window.sessionStorage.removeItem("token");
-                    window.localStorage.removeItem("token");
-                    $("#login-failed-alert").removeClass("hidden");
-                }
+            error: function () {
+                window.sessionStorage.removeItem("token");
             }
         });
-        return false; // avoid to execute the actual submit of the form.
-    });
-
-    $("#register-form").submit(function () {
-        $.ajax({
-            type: "POST",
-            url: HOST_URL[window.location.host] + "register",
-            crossDomain: true,
-            data: $("#register-form").serialize(),
-            success: function (data) {
-                if (data.error) {
-                    if (data.error === 'USERNAME_ALREADY_EXISTS') {
-                        $("#register-failed-user").removeClass("hidden");
-                    }
-                    else if (data.error === 'EMAIL_ALREADY_EXISTS') {
-                        $("#register-failed-email").removeClass("hidden");
-                    }
-                    else {
-                        $("#register-failed-alert").removeClass("hidden");
-                    }
-
-                }
-                else {
-                    $("#register-failed-alert").addClass("hidden");
-                    $("#register-failed-user").addClass("hidden");
-                    $("#register-failed-email").addClass("hidden");
-                    $('#register').modal('hide');
-                    $('#register-success').modal('show');
-                }
-            },
-            error: function (data) {
-                $("#register-failed-alert").removeClass("hidden");
-            }
-        });
-        return false; // avoid to execute the actual submit of the form.
-    });
-
-    $('#login').on('shown.bs.modal', function () {
-        $("#login-failed-alert").addClass("hidden");
-        $("#login-failed-email-notvalidated").addClass("hidden");
-        $("#login-email-validated").addClass("hidden");
-        $('#username').focus();
-    });
-
-    $('#register').on('shown.bs.modal', function () {
-        $("#register-failed-alert").addClass("hidden");
-        $("#register-failed-user").addClass("hidden");
-        $("#register-failed-email").addClass("hidden");
-    });
-
-    $(function () {
-        $('a.register-btn').click(function () {
-            $('#login .close').click();
-            $('#register').modal('show');
-        });
-    });
-
-    $(window).load(function () {
-        var token = window.sessionStorage.getItem("token")||window.localStorage.getItem("token");
-        if (token) {
-            $.ajax({
-                type: "post",
-                url: '/api/user-data',
-                headers: {
-                    "Authorization": "Bearer " + token
-                },
-                error: function () {
-                    window.sessionStorage.removeItem("token");
-                }
-            });
-            $('#login-link').addClass('hide');
-            $('#register-link').addClass('hide');
-            $('#logged-in').removeClass('hide');
-        }
-        var queryStrings = getQueryStrings();
-        if (queryStrings['promptLogin'] === 'true') {
-            $('#login').modal('show');
-        }
-        else if (queryStrings['emailValidated'] === 'true') {
-            $('#login').modal('show');
-            $('#login-email-validated').removeClass('hidden');
-        }
-        if ($('#monthlyRadio').prop('checked')) {
-            monthlyRadioClicked();
-        } else {
-            yearlyRadioClicked();
-        }
-    });
-
-    function getQueryStrings() {
-        var vars = {}, hash;
-        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-        for (var i = 0; i < hashes.length; i++) {
-            hash = hashes[i].split('=');
-            vars[hash[0]] = hash[1];
-        }
-        return vars;
+        $('#login-link').addClass('hide');
+        $('#register-link').addClass('hide');
+        $('#logged-in').removeClass('hide');
     }
-
-    function monthlyRadioClicked() {
-        $('.yearlyPrice').addClass('hide');
-        $('.monthlyPrice').removeClass('hide');
+    var queryStrings = getQueryStrings();
+    if (queryStrings['promptLogin'] === 'true') {
+        $('#login').modal('show');
     }
-
-    function yearlyRadioClicked() {
-        $('.monthlyPrice').addClass('hide');
-        $('.yearlyPrice').removeClass('hide');
+    else if (queryStrings['emailValidated'] === 'true') {
+        $('#login').modal('show');
+        $('#login-email-validated').removeClass('hidden');
     }
+    if ($('#beta1Radio').prop('checked')) {
+        beta1RadioClicked();
+    } else if ($('#beta2Radio').prop('checked')) {
+        beta2RadioClicked();
+    } else {
+        launchRadioClicked();
+    }
+});
 
-})();
+function getQueryStrings() {
+    var vars = {}, hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+function beta1RadioClicked() {
+    $('.beta1Price').removeClass('hide');
+    $('.beta2Price').addClass('hide');
+    $('.launchPrice').addClass('hide');
+}
+
+function beta2RadioClicked() {
+    $('.beta1Price').addClass('hide');
+    $('.beta2Price').removeClass('hide');
+    $('.launchPrice').addClass('hide');
+}
+
+function launchRadioClicked() {
+    $('.beta1Price').addClass('hide');
+    $('.beta2Price').addClass('hide');
+    $('.launchPrice').removeClass('hide');
+}
