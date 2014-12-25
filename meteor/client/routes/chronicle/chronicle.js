d9117dd5fcc8847f104cc831b12124da642d1744
Nicolas Maquet <nmaquet@gmail.com>
2014-12-25 15:40:46 +1300

    add / remove text

--- meteor/client/routes/chronicle/chronicle.js
+++ meteor/client/routes/chronicle/chronicle.js
@@ -4,9 +4,36 @@ Router.route('/chronicle/:_id', function () {
     if (!Meteor.user()) {
         Router.go('/')
     } else {
-        Meteor.subscribe("chronicles", Meteor.userId())
+        Meteor.subscribe("chronicles", Meteor.userId());
         this.render('chronicle', {
             data: Chronicles.findOne({_id: this.params._id})
         });
+    }
+});
+
+Template.chronicle.events({
+    'click .delete-user-content-button': function () {
+        Chronicles.update({_id: Router.current().params._id}, {$pull: {content: { lastUpdated: this.lastUpdated }}});
+    }
+});
+
+Template.addUserContentForm.events({
+    "submit form": function (event) {
+        event.preventDefault();
+        userContentType = $("#add-user-content-select").val();
+        if (userContentType === "monster") {
+            return alert("cannot yet add monsters");
+        }
+        if (userContentType === "text") {
+            var text = {
+                type: "text",
+                content: {
+                    title: "Foo title",
+                    body: "Bar body"
+                },
+                lastUpdated: new Date().toISOString()
+            };
+            Chronicles.update({_id: Router.current().params._id}, {$push: {content: text}});
+        }
     }
 });
