5237b34c62609a0b75f20e29a0cb823303814bb8
Nicolas Maquet <nmaquet@gmail.com>
2014-12-28 12:50:18 +1300

    add monster search filters

--- meteor/client/routes/search/search.js
+++ meteor/client/routes/search/search.js
@@ -3,3 +3,18 @@
 Router.route('/search', function () {
     this.render('search');
 });
+
+Template.search.events({
+    "keyup #monster-search-input": function (event) {
+        console.log(event.target.value);
+    },
+    "change #monster-type-select": function (event) {
+        console.log(event.target.value);
+    },
+    "change #monster-climate-select": function (event) {
+        console.log(event.target.value);
+    },
+    "change #monster-terrain-select": function (event) {
+        console.log(event.target.value);
+    }
+});
